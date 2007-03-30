7d1439922323463308176691470e8e84c4c6715e
Fwolf <Fwolf@0e3e1b03-7217-0410-b0ab-816a89ca1e6f>
2007-03-30 08:17:17 +0000

    NEW: Add class Curl in curl.php. NEW: Add class Mailsender in mailsender.php.
    git-svn-id: https://office.fwolf.com:82/svn/fwolflib/trunk@16 0e3e1b03-7217-0410-b0ab-816a89ca1e6f

---
+++ class/curl.php
@@ -0,0 +1,282 @@
+<?php
+/**
+* @package      fwolflib
+* @copyright    Copyright 2007, Fwolf
+* @author       Fwolf <fwolf.aide+fwolflib@gmail.com>
+*/
+
+/**
+* A class aimed to use curl function efficiency
+*
+* Very useful in write a game bot, or an information thief program.
+*
+* @package    fwolflib
+* @copyright  Copyright 2007, Fwolf
+* @author     Fwolf <fwolf.aide+fwolflib@gmail.com>
+* @since      2007-03-14
+* @version    $Id$
+*/
+class Curl
+{
+	/**
+	 * File to save cookie
+	 * @var	string
+	 * @access	protected
+	 */
+	protected $mCookiefile = '/home/fwolf/log/curl_cookie.txt';
+
+	/**
+	 * File to save log
+	 * Set to empty string to direct echo out(default),
+	 * Or set to a file to save in,
+	 * Or set to /dev/null to do nothing.
+	 * @var	string
+	 * @access	public
+	 */
+	public $mLogfile = '';
+
+	/**
+	 * Result read from webserver
+	 * @var	string
+	 * @access	public
+	 */
+	public $mRs = '';
+
+	/**
+	 * Curl session resource
+	 * @var	object
+	 * @access	public
+	 */
+	public $mSh;
+
+
+	/**
+	 * Construct function
+	 * @access public
+	 */
+	function __construct()
+	{
+		$this->mSh = curl_init();
+		$this->SetoptCommon();
+	} // end of func construct
+
+
+	/**
+	 * Destruct function
+	 * @access public
+	 */
+	function __destruct()
+	{
+		curl_close($this->mSh);
+	} // end of func destruct
+
+
+	/**
+	 * Http get content from host
+	 * @param	string	$url	Host address
+	 * @param	mixed	$param	Get parameter, can be string or array.
+	 * @access	public
+	 * @return	string
+	 */
+	public function Get($url, $param = '')
+	{
+		curl_setopt($this->mSh, CURLOPT_HTTPGET, true);
+
+		// Remove endding '?" of url
+		if ('?' == substr($url, -1, 1))
+			$url = substr($url, 0, strlen($url - 1));
+
+		// Char used between url & param
+		if (false === strpos($url, '?'))
+			$s_linker = '?';
+		else
+			$s_linker = '&';
+
+		// Parse param, join array and fix linker char with url
+		if (is_array($param))
+			$param = $s_linker . implode('&', $param);
+		else
+			if ('&' == substr($param, 0, 1))
+				$param{0} = $s_linker;
+
+		curl_setopt($this->mSh, CURLOPT_URL, $url . $param);
+		$this->mRs = curl_exec($this->mSh);
+
+		if (0 != curl_error($this->mSh))
+			$this->Log(curl_error($this->mSh));
+
+		return $this->mRs;
+	} // end of func Get
+
+
+	/**
+	 * Output log
+	 * @param	string	$log	Content of log
+	 * @access	public
+	 */
+	public function Log($log = '')
+	{
+		if (empty($log))
+			return;
+
+		$s_date = date('Y-m-d H:i:s');
+		$log = "[$s_date] $log\n";
+
+		if (empty($this->mLogfile))
+			echo $log;
+		else
+			file_put_contents($this->mLogfile, $log, FILE_APPEND);
+	} // end of func Log
+
+
+	/**
+	 * Match content to variables using preg
+	 * To read content currectly, content parsing is nesessary
+	 * Return value maybe string or array, use careful and
+	 *   remind which value you use it for.
+	 * @param	string	$preg
+	 * @return	mixed
+	 * @access	public
+	 */
+	public function Match($preg)
+	{
+		if (empty($preg)) return '';
+		$i = preg_match_all($preg, $this->mRs, $ar, PREG_SET_ORDER);
+		if (0 == $i || false === $i)
+			// Got none match or Got error
+			$ar = '';
+		elseif (1 == $i)
+		{
+			// Got 1 match, return as string or array(2 value in 1 match)
+			$ar = $ar[0];
+			array_shift($ar);
+			if (1 == count($ar))
+				$ar = $ar[0];
+		}
+		else
+		{
+			// Got more than 1 match return array contains string or sub-array
+			foreach ($ar as &$row)
+			{
+				array_shift($row);
+				if (1 == count($row))
+					$row = $row[0];
+			}
+		}
+		return $ar;
+	} // end of func Match
+
+
+	/**
+	 * Http post content from host
+	 * @param	string	$url	Host address
+	 * @param	mixed	$param	Get parameter, can be string or array.
+	 * @access	public
+	 * @return	string
+	 */
+	public function Post($url, $param = '')
+	{
+		curl_setopt($this->mSh, CURLOPT_POST, true);
+
+		// Parse param, convert array to string
+		if (is_array($param))
+		{
+			$s = '';
+			foreach ($param as $key=>$val)
+				$s .= "$key=$val&";
+			$param = $s;
+		}
+
+		curl_setopt($this->mSh, CURLOPT_POSTFIELDS, $param);
+		curl_setopt($this->mSh, CURLOPT_URL, $url);
+		$this->mRs = curl_exec($this->mSh);
+
+		if (0 != curl_error($this->mSh))
+			$this->Log(curl_error($this->mSh));
+
+		return $this->mRs;
+	} // end of func Post
+
+
+	/**
+	 * Set some common options using curl_setopt
+	 * @access	public
+	 */
+	public function SetoptCommon()
+	{
+		$this->SetoptCookie();
+		$this->SetoptUseragent('ie6');
+
+		curl_setopt($this->mSh, CURLOPT_AUTOREFERER, true);
+		// If got http error, report.
+		curl_setopt($this->mSh, CURLOPT_FAILONERROR, true);
+		curl_setopt($this->mSh, CURLOPT_FOLLOWLOCATION, true);
+		// Return result restead of display it.
+		curl_setopt($this->mSh, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($this->mSh, CURLOPT_CONNECTTIMEOUT, 300);
+		curl_setopt($this->mSh, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+		curl_setopt($this->mSh, CURLOPT_MAXREDIRS, 10);
+		curl_setopt($this->mSh, CURLOPT_TIMEOUT, 300);
+	} // end of func SetoptCommon
+
+
+	/**
+	 * Set cookie option
+	 * If filename is not given, use default,
+	 * If file is given, use & set it as default.
+	 * @param	string	$cookiefile
+	 * @access	public
+	 */
+	public function SetoptCookie($cookiefile = '')
+	{
+		if (!empty($cookiefile))
+			$this->mCookiefile = $cookiefile;
+		curl_setopt($this->mSh, CURLOPT_COOKIEFILE, $this->mCookiefile);
+		curl_setopt($this->mSh, CURLOPT_COOKIEJAR, $this->mCookiefile);
+	} // end of func SetoptCookie
+
+
+	/**
+	 * Set proxy option
+	 * @param	int		$ptype	0-no proxy, 1-http, 2-socks5
+	 * @param	string	$phost
+	 * @param	int		$pport
+	 * @param	string	$pauth	[username]:[password]
+	 * @access	public
+	 */
+	public function SetoptProxy($ptype, $phost, $pport, $pauth = '')
+	{
+		if (0 == $ptype)
+			curl_setopt($this->mSh, CURLOPT_HTTPPROXYTUNNEL, false);
+		else
+		{
+			curl_setopt($this->mSh, CURLOPT_HTTPPROXYTUNNEL, true);
+			curl_setopt($this->mSh, CURLOPT_PROXY, $phost);
+			if (1 == $ptype)
+				curl_setopt($this->mSh, CURLOPT_PROXYTYPE, CURLPROXY_HTTP);
+			if (2 == $ptype)
+				curl_setopt($this->mSh, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
+			curl_setopt($this->mSh, CURLOPT_PROXYPORT, $pport);
+			if (!empty($pauth))
+				curl_setopt($this->mSh, CURLOPT_PROXYUSERPWD, $pauth);
+		}
+	} // end of func SetoptProxy
+
+
+	/**
+	 * Set browser agent option
+	 * @param	string	$browser
+	 * @access	public
+	 */
+	public function SetoptUseragent($browser)
+	{
+		$b['ie6'] = 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)';
+		$b['googlebot'] = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)';
+
+		if (isset($b[$browser]))
+			curl_setopt($this->mSh, CURLOPT_USERAGENT, $b[$browser]);
+	} // end of func SetoptUseragent
+
+
+} // end of class Curl
+?>
---
+++ class/mailsender.php
@@ -0,0 +1,349 @@
+<?php
+/**
+* @package      fwolflib
+* @copyright    Copyright 2007, Fwolf
+* @author       Fwolf <fwolf.aide+fwolflib@gmail.com>
+*/
+
+require_once("phpmailer/class.phpmailer.php");
+
+/**
+* Mail Sender
+*
+* Subclass of PHPMailer, make it easier to use.
+*
+* @package    fwolflib
+* @copyright  Copyright 2007, Fwolf
+* @author     Fwolf <fwolf.aide+fwolflib@gmail.com>
+* @since      2007-03-29
+* @version    $Id$
+*/
+class Mailsender extends PHPMailer
+{
+	/**
+	 * Mail attachment
+	 * Array of string
+	 * @var	array
+	 */
+	public $mAttach = array();
+
+	/**
+	 * Mail body
+	 * @var	string
+	 */
+	public $mBody = '';
+
+	/**
+	 * Charset of mail
+	 * @var	string
+	 */
+	public $mCharset = 'utf-8';
+
+	/**
+	 * Encoding method of mail body
+	 * @var	string
+	 */
+	public $mEncoding = 'base64';
+
+	/**
+	 * Error count
+	 * Reset when mail send success
+	 * @var	int
+	 */
+	public $mErrorCount = 0;
+
+	/**
+	 * Error message
+	 * Reset when mail send success
+	 * @var string
+	 */
+	public $mErrorMsg = '';
+
+	/**
+	 * Mail from address
+	 * @var	string
+	 */
+	public $mFrom = '';
+
+	/**
+	 * Mail from name
+	 * @var	string
+	 */
+	public $mFromName = 'Aliens';
+
+	/**
+	 * Mail host
+	 * Don't include port number
+	 * @see	$mPort
+	 * @var	string
+	 */
+	public $mHost = '';
+
+	/**
+	 * Html format mail ?
+	 * @var	boolean
+	 */
+	public $mIsHtml = false;
+
+	/**
+	 * Auth type
+	 * Smtp default.
+	 * @var	string
+	 */
+	public $mIsSmtp = true;
+
+	/**
+	 * Pass to login mail host
+	 * @var	string
+	 */
+	public $mPass = '';
+
+	/**
+	 * Mail host port number
+	 * @var	int
+	 */
+	public $mPort = 25;
+
+	/**
+	 * Keep smtp connection to furture useage ?
+	 * Var in phpmailer is $SMTPKeepAlive default false
+	 * @var	boolean
+	 */
+	public $mSmtpKeepAlive = false;
+
+	/**
+	 * Subject of mail
+	 * @var	string
+	 */
+	public $mSubject = '';
+
+	/**
+	 * Address to be mailed to
+	 * Parsed data, always is an array
+	 * @var	array
+	 */
+	public $mTo = array();
+
+	/**
+	 * Username on mail host
+	 * Some host is xxx, while some is xxx@mail.com
+	 * @var	string
+	 */
+	public $mUser = '';
+
+
+	/**
+	 * Construct
+	 */
+	public function __construct()
+	{
+		//parent::PHPMailer();
+	} // end of func construct
+
+
+	/**
+	 * Parse mail to address
+	 * Input any type address, output standard array of address
+	 * Parse string including email name and address to address=>name array.
+	 *
+	 * @var	mixed	$to
+	 * return	array
+	 */
+	public function ParseTo($to)
+	{
+		if (is_array($to))
+			return $to;
+		else
+		{
+			//$addresses = ',,,;;; "@1>"1<1 ,\';" <1@1.com.cn> , 2@2.com, f <f@f.com.cn> ,,39@39.cc,;;; ';
+			//$addresses = ', fwolfcn@gmail.com';
+
+			//First, find all mail address out
+			$j = preg_match_all('/[\s<]?([\w\d\-_\.\+]+@([\w\d\-_]+\.){1,4}\w+)[\s>]?/', $to, $addr_addr);
+
+			//if got addresses, find names according there position in string
+			$addr = array();
+			if (0 < $j)
+			{
+				$addr_addr = $addr_addr[1];
+				for ($i=0; $i<$j; $i++)
+				{
+					//this can always find
+					$k = strpos($to, $addr_addr[$i]);
+					$name = substr($to, 0, $k);
+					//prepare for next loop
+					$to= substr($to, $k + strlen($addr_addr[$i]));
+					//trim string we parsed out
+					$name = trim($name, ' \t<>;,"');
+					//gerenate addr array like address=>name style
+					$addr[$addr_addr[$i]] = $name;
+				}
+				//foreach ($addr as $key=>$val)
+				//	echo $key . '=>' . $val . "\n";
+			}
+			return($addr);
+		}
+	} // end of func ParseTo
+
+
+	/**
+	 * Prepare - Common setup
+	 */
+	public function Prepare()
+	{
+		// NOTICE --> Char'S'et
+		$this->CharSet = $this->mCharset;
+		$this->Encoding = $this->mEncoding;
+		if (true == $this->mIsSmtp)
+			$this->IsSMTP();
+		if (true == $this->mIsHtml)
+			$this->IsHTML(true);
+		else
+			$this->IsHTML(false);
+	} // end of func Prepare
+
+
+	/**
+	 * Send mail
+	 * @param	string	$from	Only xxx@mail.com format, no fromname
+	 * @param	mixed	$to
+	 * @param	string	$subject
+	 * @param	string	$body
+	 * @param	mixed	$attach
+	 * @return	boolean
+	 */
+	public function Send($from = '', $to = '', $subject = '', $body = '', $attach = '')
+	{
+		$this->Prepare();
+		if (!empty($from))
+			$this->SetFrom($from);
+		if (!empty($to))
+			$this->SetTo($to);
+		if (!empty($subject))
+			$this->SetSubject($subject);
+		if (!empty($body))
+			$this->SetBody($body);
+		if (!empty($attach))
+			$this->SetAttach($attach);
+
+		$sok = parent::Send();
+		if (true == $sok)
+		{
+			$this->mErrorCount ++;
+			$this->mErrorMsg = $this->ErrorInfo;
+		}
+		else
+		{
+			$this->mErrorCount = 0;
+			$this->mErrorMsg = '';
+		}
+
+		if (false == $this->mSmtpKeepAlive)
+			$this->SmtpClose();
+		return $sok;
+	} // end of function Send
+
+
+	/**
+	 * Set mail attachment
+	 * @param	mixed	$attach
+	 */
+	public function SetAttach($attach)
+	{
+		if (is_array($attach))
+		{
+			$this->mAttach = &$attach;
+			$this->ClearAttachments();
+			foreach ($attach as $att)
+				$this->AddAttachment($att);
+		}
+		elseif (!empty($attach))
+		{
+			$this->mAttach = array($attach);
+			$mail->AddAttachment($attach);
+		}
+	} // end of func SetAttach
+
+
+	/**
+	 * Set host auth information
+	 * @param	string	$userid
+	 * @param	string	$passwd
+	 */
+	public function SetAuth($userid, $passwd)
+	{
+		$this->mUser = $userid;
+		$this->mPass = $passwd;
+		$this->Username = $this->mUser;
+		$this->Password = $this->mPass;
+	} // end of func SetAuth
+
+
+	/**
+	 * Set mail body content
+	 * @param	string	$body
+	 */
+	public function SetBody($body)
+	{
+		$this->mBody = $body;
+		$this->Body = $this->mBody;
+	} // end of func SetBody
+
+
+	/**
+	 * Set from & from name
+	 * @param	string	$from
+	 * @param	string	$fromname
+	 */
+	public function SetFrom($from, $fromname = 'Aliens')
+	{
+		$this->mFrom = $from;
+		$this->mFromName = $fromname;
+		$this->From = $this->mFrom;
+		$this->FromName = $this->mFromName;
+	} // end of func SetFrom
+
+
+	/**
+	 * Set host information
+	 * @param	string	$addr
+	 * @param	int		$port
+	 * @param	boolean	$issmtp
+	 */
+	public function SetHost($addr, $port = '25', $issmtp = true)
+	{
+		$this->mHost = $addr;
+		$this->mPort = $port;
+		$this->mIsSmtp = $issmtp;
+		$this->Host = $this->mHost;
+		$this->Port = $this->mPort;
+		$this->SMTPAuth = $this->mIsSmtp;
+	} // end of func SetHost
+
+
+	/**
+	 * Set mail subject
+	 * @param	string	$sub
+	 */
+	public function SetSubject($sub)
+	{
+		$this->mSubject = $sub;
+		$this->Subject = $this->mSubject;
+	} // end of func SetSubject
+
+
+	/**
+	 * Set address to mail to
+	 * @param	mixed	$to
+	 */
+	public function SetTo($to)
+	{
+		$to_ar = $this->ParseTo($to);
+		$this->mTo = &$to_ar;
+		$this->ClearAddresses();
+		foreach ($to_ar as $key => $val)
+			$this->AddAddress($key, $val);
+	} // end of func SetTo
+
+}
+?>
