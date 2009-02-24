82ab59a6da5854389ecf6f413b75b0253d699320
Fwolf <Fwolf@0e3e1b03-7217-0410-b0ab-816a89ca1e6f>
2009-02-24 04:24:27 +0000

    NEW: Remove ':000' before original strtotime().
    git-svn-id: https://office.fwolf.com:82/svn/fwolflib/trunk@83 0e3e1b03-7217-0410-b0ab-816a89ca1e6f

---
+++ func/datetime.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Provide date or time relate function
+ * @package     fwolflib
+ * @copyright   Copyright 2009, Fwolf
+ * @author      Fwolf <fwolf.aide+fwolflib-func@gmail.com>
+ * @since		2009-02-24
+ * @version		$Id$
+ */
+
+
+/**
+ * strtotime added remove of ':000' in sybase time(probably because dblib)
+ * @param	string	$time
+ * @return	integer
+ */
+function Strtotime1($time) {
+	if (!empty($time)) {
+		$time = str_replace(':000', '', $time);
+	}
+	return strtotime($time);
+} // end of func Strtotime1
+
+?>
