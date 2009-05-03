78e89cfe9ec3ba24f80e92f0e5d76ba12b76f2a8
Fwolf <fwolf.aide+git@gmail.com>
2009-05-04 02:46:59 +0800

    Add doc: rule-english.php.

---
+++ doc/rule-english.php
@@ -0,0 +1,284 @@
+<?php
+/**
+ * English Words Common Used in Develop
+ *
+ * @package		fwolflib
+ * @subpackage	doc
+ * @copyright	Copyright 2009, Fwolf
+ * @author		Fwolf <fwolf.aide+fwolflib.doc@gmail.com>
+ * @since		2009-05-04
+ * @version		$Id$
+ */
+
+$ar_info = array();
+$ar_info['title'] 		= 'English Words Common Used in Develop';
+$ar_info['author']		= 'Fwolf';
+$ar_info['authormail'] 	= 'fwolf.aide+fwolflib.doc@gmail.com';
+$ar_info['keywords']	= 'doc, rules, english, abbr';
+$ar_info['description']	= '常用英文及缩写。';
+
+$ar_body = array();
+$ar_body[] = '
+<div class="single_line" markdown="1">
+
+## 词汇表
+
+单词、缩写					|	含义
+----------------------------|-------------
+abnormal, abn				|不正常的
+achievements				|业绩
+address, addr				|地址
+aggregate, agg				|汇总，侧重于不是简单数学累加的情况
+allocate, alloc, alo		|分配，侧重于把一个整体划分到各个单体，比如把总分分配到每个子项上
+amount, amt					|总数，数量，有时即可指数量，也可指总合计，所以慎用，替代词：quantity
+announcement, announce, anc	|公告
+application					|用途
+application, appl			|申请（名词）
+apply						|申请，报名
+approve, aprv				|审核
+approved, apvd				|已审核通过
+approved by / checked by / prepared by	|核准/审核/承办
+bank capacity letter, bank conform letter, bcl	|银行资金证明
+bank guarantee				|银行保函
+below cost, bc				|低于成本
+bid bond, bid security		|投标保证金
+bid evaluation committee	|评标委员会
+bid inviting				|招标（邀请）
+bid issue date, bid			|发标日期
+bid opening, bo				|开标
+bid section					|招标标段
+bidder						|投标人
+bidding agency, bagy		|招标代理（机构）
+bidding document, bid_doc	|招标文件
+bidding document edit system, bdes	|标书制作软件/系统
+bidding evaluation, be		|评标，标书评审
+bidding evaluation assistance system, beas	|辅助评标系统，评标辅助系统
+bidding evaluation experts, bee	|评标专家
+bidding evaluation plan, bep|评标办法，参考：评标标准 evaluation criteria，也有用bid evaluation method的，不过我们将method定义为范围相对小的情况，比如评分采用低价法时，使用method。描述整体评标办法，仍然使用plan。
+bill of quantities, boq		|工程量清单
+	priced bill of quantities, boq_priced?	|工程量清单报价（名词，工程量报价清单？）
+brand						|商标，品牌
+boundary line, red line		|控制线/边界线/分界线
+boundary price, bp			|价格控制线
+build, operate, transfer, bot	|建设－拥有－转让
+building area				|建筑面积
+business license, bl		|营业执照
+	bl_en					|经济性质
+	bl_main					|主营
+	bl_rc					|注册资金，单位：万元
+	bl_rno					|营业执照注册号
+	bl_sideline				|兼营
+calculate, calc				|计算，和compute相比，特指用数学方法进行比较复杂、难度较大的精确计算
+category, cat, ctgy			|类别，分类
+certificate, cert			|证书，证明
+checker						|审核人，检查人
+class						|等级，级别
+commercial bid, cb			|商务标
+comparison and choice, cc	|比选，比较、选择
+composing					|排版
+comprehensive evaluation method, cem	|综合评估法
+construction, cons			|施工，建设
+	construction bureau, cb	|建设局
+	construction engineering trading center, cetc	|建设工程交易中心
+	construction engineering bidding and tendering manage office, cebtmo, cbtmo, bmo	|建设工程招投标管理办公室，招标办
+	construction engineering bidding and tendering management, cebtm, btm	|招投标管理
+	construction enterprise, cent	|施工企业
+	construction period, period	|工期
+	construction scale			|建设规模
+	construction tendering and trading management system, cttms	|建设工程招投标交易管理系统(按照tender->bid的转换，应当使用cbtms更适宜，下次改过)
+constructor					|（一级）建造师
+	associate constructor	|二级建造师
+contact, ctc				|联系人
+contract, cnt				|合同
+creator						|创建者，录入人
+degree						|学位
+delivery					|发送
+department, dept			|部门
+	department of construction, doc	|建设厅
+description, desc			|（产品）描述
+detail, dtl					|详细信息
+determine the winning bidder, dtwb	|确定中标人
+diploma						|学历
+direct contract， contracted out directly	|直接发包
+economic nature, en			|经济性质
+eligible bidders			|合格的投标人
+electronic bidding document, ebd	|电子招标文件
+electronic tendering document, etd	|电子投标文件
+employer					|雇主，工作单位
+end date, ed				|结束日期
+end time, et				|结束时间
+enterprice, ent				|企业
+enterprice-establishing		|招商
+engineering, eng			|工程，范围比项目小
+estimate, est				|预计
+evaluated lowest bid price method, elbpm	|经评审的最低投标价法，也可简单绎为lowest bid method, lpm，与综合评估法cem相对应。
+evaluation meeting, em		|评审会
+exceed limit, el			|超过比例
+expert evaluation, ee		|专家评审
+fee							|费用，规费
+fraud						|欺诈
+great_than, gt				|大于或等于（bash中的用法）
+history, his, hist, hx		|历史
+investment, invst			|投资
+investment nature, invst_nature	|投资性质
+item No.					|产品标号
+job title, jtitle			|工作职务
+lecture						|讲课稿
+legal representative, lgr	|法定代表人
+less than, lt				|小于或等于（bash中的用法）
+lower limit, ll				|下限
+lowest price, lp			|最低价
+	next (to the) lowest price, nlp	|次低价
+main material, mm			|主要材料
+management, mgmt			|管理
+manager, mgr				|管理人员，管理者，经理
+manufacturer				|生产厂家
+measurement item, mi		|措施项目
+ministry of construction, moc	|建设部
+mode						|方式
+mode of operation, moo		|经营方式
+model						|型号
+modifier					|修改人
+notify						|通知
+number of employees, noe	|从业人数
+offer period				|要约期
+owner						|建设单位
+opinion						|(评审)意见，评论
+percent, pct				|比例（值）
+performance bond, pb		|履约保证金
+person, per					|人员
+personal identification number, pin	|身份证号，个人识别码
+phone, fon, phn				|[联系]电话
+place, pl					|地点
+post review					|事后审查
+postcode					|邮编
+potential bidder			|潜在投标人
+pre-qualification submission	|投标资格预审申请书
+preliminary evaluation, pe	|初步评审
+prior review				|事前审查
+privilege, priv				|权限
+production place			|产地
+professional title, ptitle	|职称
+profile, prof, pf			|简介，基本情况
+programming					|规划
+project, proj				|项目，范围比工程大
+project application formalities	|(项目)报建手续
+project manager, pm			|项目经理
+proof of funds, pof			|资金证明
+province, prov, pr			|省
+publication, public, pub	|公示
+purview						|范围
+qualification, qual			|资质，资格
+	postqualification		|资格后审
+	prequalification		|资格预审
+	qual_by					|发证机关/部门
+	qual_level				|资质等级
+	qual_rno				|资质证书编号
+	qual_time				|取得资格时间
+qualified bidder			|合格的投标人
+qualified bidder definition	|合格的投标人定义？（标准？）
+qualified bidder\'s list, qbl	|合格的投标人列表
+quality, **qlty**, qual		|质量
+quantity, qnty				|数量，可用来替代amount
+quote						|报价（动词）
+random select, rs			|随机抽取
+reasonable range, rr		|合理范围
+recommendation				|推荐意见
+region						|地区
+register, reg				|登记
+registered capital, rc		|注册资金
+registered number, rno		|注册号
+registered number of approval, approval number, ano	|批准文号
+reject, rej					|退回
+rejected bid, rb			|废标
+required, req, r			|需要的，必需的
+scale						|（公司）规模
+scientific outlook on development, sood	|科学发展观
+scope						|范围，是数据库中的关键字，需要和其他单词组合使用
+scope of business, sob		|经营范围
+self service, ss			|自助服务
+sequence, seq				|顺序
+sign in						|签到
+size						|尺寸
+source of funds, sof?		|资金来源
+special report				|专题报道
+speciality					|（所学）专业
+	speciality_major		|所学专业
+	speciality_work			|从事专业
+specification, spec			|规格，描述，内容
+sporadic					|零星的，不定时发生的
+standard price, sp			|标准价，基准价
+start date, sd				|开始日期
+start time, st				|开始时间
+submit, subm				|提交
+submission, subm			|申请书
+substantial response		|实质性响应
+supervision					|监督，管理
+supervision suggestion order, sso	|监督意见(书)
+supervisor					|监理
+symposium					|座谈会
+technical bid, tb			|技术标
+technique data				|技术指标
+telephone, tel				|电话（联系方式）
+tenderee					|招标人
+tenderer					|投标人*
+tender document				|招标文件*
+tendering document, tender_doc	|投标文件，内部固定用法，T/B两词实在不好区分
+tender evaluation, te		|评标，标书评审*
+tender evaluation plan, tep	|评标办法*
+tender submission			|投标申请书*
+three supplies and one leveling, tsaol	|三通一平
+time of formation, tof		|成立时间
+time period, tmper			|时间段，时限，周期
+timestamp, ts				|时间戳
+total capital investment, tci	|总投资（资本投资）
+	tci_cy(current year)	|本年投资
+total price, tp				|总价，总报价
+trend(s)					|（行业）趋势，动态
+upper limit, ul				|上限
+works category				|工程类别
+
+## 近义词
+
+### bid/tender现在应该以用bid为主，例外：招标人/tenderee
+
+- [招投标：Tender 与 Bid 之异同](http://bbs.chinabidding.com/simple/index.php?t12172.html)
+- [投标（文件）＆招标（文件）这个两个词怎么译](http://www.fane.cn/forum_view.asp?forum_id=34&view_id=15492)
+- [国际招标与投标英语词汇](http://bbs.chinabidding.com/simple/index.php?t14033.html)
+
+### subject/较概括、笼统的话题 topic/限定的主题 title/静态的标题或题目
+
+* [Subject Topic Title ](http://ks.cn.yahoo.com/question/1307013006553.html)
+
+### 建设单位/施工单位/监理单位/物业管理
+
+- 施工单位 Construction unit
+- 监理单位 Supervision unit
+- 建设单位 Construction unit, 英语的"施工单位"和"建设单位"的表达是相同的, 通常翻译为owner
+- 物业管理 Estate Management
+
+## 参考：
+
+* [企业网站常用中英文对照表](http://www.cnbruce.com/blog/showlog.asp?log_id=974)
+
+
+## 不宜使用的单词
+
+一些在数据库设计中<br />不宜使用的单词	|替代词
+------------------------------------|------
+action				|
+bit					|
+date				|
+enum				|
+name				|title 即使是在组合词中也应避免使用name，因为它可能存在的地方太多了
+no					|
+note <a href="" onclick="return false;" title="(用PD设计ACCESS库，无法Modify Database)">?</a>		|description, remark
+scope				|purview
+text				|content
+time				|
+timestamp			|lasttime
+value				|intvalue, strvalue, datevalue
+
+</div>
+';
+?>
