086a9967ba0e69a97d9d09663dbffb79d394d222
Fwolf <fwolf.aide+git@gmail.com>
2009-05-11 21:47:49 +0800

    Chg a little format auto adjust by Geany .

--- class/mvc-controler.php
+++ class/mvc-controler.php
@@ -10,7 +10,7 @@ require_once('fwolflib/func/request.php');
 require_once('fwolflib/func/string.php');
 
 /*
-// In subclass or subclass for an app, 
+// In subclass or subclass for an app,
 // the bottom layer class should define P2R first at here, and in this way:
 if (!defined('P2R')) define('P2R', './');
 // Then you can use P2R to require some app libs.
@@ -20,19 +20,19 @@ if (!defined('P2R')) define('P2R', './');
  * Controler class in MVC
  *
  * 控制系统的哪些功能被调用，主要是对应主系统和子系统根下的index.php文件，这些文件始终是用户调用的文件，对于每个子系统也是这样。同理，全局常量P2R在Contoler中定义。
- * 
+ *
  * 主系统根下的index.php通过`\$_GET['m']`(module)来确定应该引用哪个子系统的index.php，相当于“进入某个子系统”。
- * 
+ *
  * 子系统根下的index.php通过`\$_GET['a']`(action)来调用哪个功能相应的Page，类似的功能可以是几个action都在一个页面中处理。
- * 
+ *
  * 主系统和子系统下的index.php要做到都可以被单独调用。
- * 
+ *
  * 如果子系统的功能比较多，还可以再设计一层Controler，用于实现功能的选择；也可以采用统一放入子系统的Controler中集中控制的方式。特殊情况下，也可以从主系统根直接调用功能Page。
- * 
+ *
  * 一般项目中，Controler只起到了用户命令的分流作用，处理较少，大量的页面生成、参数转换和传递都放到了View中。也正因为如此，如果启用了缓存机制，打算将缓存放在Controler中实现。
- * 
+ *
  * 这里所说的module和action主要是针对系统逻辑的，不要和MVC中的概念混淆。
- * 
+ *
  * @package		fwolflib
  * @subpackage	mvc
  * @copyright	Copyright 2008, Fwolf
@@ -50,40 +50,40 @@ abstract class Controler
 	 * @see $fTimeStart
 	 */
 	protected $fTimeEnd = 0;
-	
+
 	/**
 	 * Run start time, used to caculate run time length
-	 * 
+	 *
 	 * Can only count to time when echo output.
 	 * @var	float
 	 * @see $fTimeEnd
 	 */
 	protected $fTimeStart = 0;
-	
+
 	/**
 	 * View object
 	 * @var	object
 	 * @see ViewDisp()
 	 */
 	protected $oView = null;
-	
+
 	/**
 	 * Action parameter, the view command to determin what to display
 	 * @var string	// $_GET['a'], means which action user prefered of the module
 	 */
 	protected $sAction = '';
-	
+
 	/**
 	 * Current module param
 	 * @var	string	// $_GET['m'], means which module user prefered
 	 */
 	protected $sModule = '';
-	
-	
+
+
 	abstract public function ViewErrorDisp($msg);	// Display view show error msg
 	abstract public function Go();	// User call starter function
-	
-	
+
+
 	/**
 	 * contruct
 	 */
@@ -91,24 +91,24 @@ abstract class Controler
 	{
 		// Record run start time first
 		$this->fTimeStart = microtime(true);
-		
+
 		// Get major parameters
 		$this->sModule = GetGet('m');
 		$this->sAction = GetGet('a');
 
 	} // end of func __construct
-	
-	
+
+
 	/**
 	 * Set run time length and db query times to View to display out.
-	 * 
-	 * Need fwolflib::View, 
+	 *
+	 * Need fwolflib::View,
 	 * and use global var $i_db_query_times set in fwolflib::Adodb::CountDbQueryTimes
-	 * 
+	 *
 	 * Assign action is done in View::GenFooter.
-	 * 
+	 *
 	 * Cost about 0.05 more second when have db query.
-	 * 
+	 *
 	 * Eg: Processed in 0.054994 seconds, 6 db queries.
 	 * @param	object	&$view	View object
 	 * @global	int		$i_db_query_times
@@ -117,7 +117,7 @@ abstract class Controler
 	public function SetInfoRuntime(&$view)
 	{
 		global $i_db_query_times;
-		
+
 		// Record run end time
 		$this->fTimeEnd = microtime(true);
 		// Generate info str, time used
@@ -129,21 +129,21 @@ abstract class Controler
 		if (isset($i_db_query_times))
 			$s .= ", $i_db_query_times db queries";
 		$s .= '.';
-		
+
 		// Assign msg to View's template object
 		if (!is_null($view->oTpl))
 			$view->oTpl->assign('debug_info_runtime', $s);
 	} // end of func SetInfoRuntime
-	
-	
+
+
 	/**
 	 * Call a view class, display it's output
-	 * 
+	 *
 	 * Result echo out directly
 	 * @param	string	$view	View define class file
 	 * @param	string	$class	View class name, if obmit, will remove '_'&'-'
 	 * 								in filename and use ucfirst($view) as class
-	 * 								name. 
+	 * 								name.
 	 * 							Auto remove beginning `v-` from $view is
 	 * 							optional, if you use 'v-view.php' naming style,
 	 * 							it will auto happen.
@@ -154,37 +154,37 @@ abstract class Controler
 		// Check file existence
 		if (file_exists($view))
 			require_once($view);
-		else 
+		else
 			$this->ViewErrorDisp("View define file $view not found!");
-			
+
 		// From ..../page_a.php, get 'page_a'.
 		$s_view = substr($view, strrpos($view, '/') + 1);
 		$s_view = substr($s_view, 0, strrpos($s_view, '.'));
-		
+
 		// Remove 'v-' from 'v-view.php', optional
 		if ('v-' == substr($s_view, 0, 2))
 			$s_view = substr($s_view, 2);
-		
+
 		// Then, 'page_a' to 'PageA'
 		// Replace '-' in view name to '_'
 		if (empty($class))
 			$class = 'View' . StrUnderline2Ucfirst($s_view, true);
-		
+
 		if (class_exists($class))
 		{
 			$this->oView = new $class($this);
 			//$p->oCtl = $this;	// Set caller object	// Moved to __contruct of View class, transfer $this when do new().
-			
+
 			echo $this->oView->GetOutput();
 		}
-		else 
+		else
 		{
 			// Display error
 			$this->ViewErrorDisp("View class $class not found!");
 		}
 	} // end of func ViewDisp
-	
-	
+
+
 } // end of class Controler
 
 ?>
