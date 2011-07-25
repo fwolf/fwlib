a65378d7329a6fada581d9ac0a76ef67c44e89de
Fwolf <fwolf.aide+git@gmail.com>
2011-07-26 01:53:57 +0800

    New css/default.css.
    Also, css and js file can directly use by app,
    	with a simple dummy loader script(eg: php) file.

--- js/common.js
+++ js/common.js
@@ -1,112 +1,101 @@
 /**
- * 通用JS函数集
+ * Js functions for common usage.
  *
- * @package    MaGod
- * @copyright  Copyright 2003-2004, Fwolf
- * @author     Fwolf <fwolf001@tom.com>
- * @since      2004-1-2 10:31:07
- * @access     public
- * @version    $Id$
+ * Using jQuery now, many are useless now.
+ *
+ * @package		fwolflib
+ * @subpackage	js
+ * @copyright	Copyright © 2011, Fwolf
+ * @author		Fwolf <fwolf.aide+fwolflib.js@gmail.com>
+ * @since		2011-07-26
  */
 
 
 /**
  * 取得某一对象制定部分的值
+ *
  * @param   string  obj     目标对象的名称
  * @param   string  part    取值部分
  * @return  string
  */
-function GetData(obj, part)
-{
-    if ( 'value' == part )
-    {
+function GetData (obj, part) {
+    if ('value' == part) {
         return GetElement(document, obj).value;
     }
-    if ( 'innerHTML' == part )
-    {
+    if ('innerHTML' == part) {
         return GetElement(document, obj).innerHTML;
     }
-	if ( 'src' == part )
-	{
+	if ('src' == part) {
 		return GetElement(document, obj).src;
 	}
-    if ( 'text' == part )
-    {
+    if ('text' == part) {
         return GetElement(document, obj).text;
     }
-} // end of function GetData
+} // end of func GetData
 
 
 /**
- * 解决JS兼容性的函数，取得某一控件的句柄
- * 在调用的时候把document对象传进去就OK了
+ * 解决 js 兼容性的函数，取得某一控件的句柄
+ * 在调用的时候把 document 对象传进去就OK了
+ *
  * @param   object  parentobj   父对象
  * @param   string  id          子对象的ID
  * @return  handle
  */
-function GetElement(parentobj, id)
-{
+function GetElement (parentobj, id) {
     var obj;
     if (!parentobj) return null;
-    if (parentobj.getElementById)
-    {
+    if (parentobj.getElementById) {
         obj = parentobj.getElementById(id);
     }
-    else if ( parentobj.all)
-    {
+    else if (parentobj.all) {
         obj = parentobj.all[id];
     }
-    else if (parentobj.layers)
-    {
+    else if (parentobj.layers) {
         obj = parentobj.layers[id];
     }
     return obj;
-} // end of function GetElement
+} // end of func GetElement
 
 
 /**
- * 打开一个新窗口，并返回FALSE
+ * 打开一个新窗口，并返回 false
  *
  * @param   string  url     地址
  * @param   string  winname 窗口名称
  * @param   string  spec    窗口打开参数
  * @param   boolean focus   打开窗口后是否自动显示在前台
+ * @return	boolean
  */
-function OpenWindow(url, winname, spec, focus)
- {
+function OpenWindow (url, winname, spec, focus) {
 //  var newwin=window.open(url,"select_jsdw","toolbar=no,Location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=400,height=550,left=50,top=50");
 //  var newwin=window.open(url,"","");
-  var win = window.open(url, winname, spec);
-  if (true == focus)
-  {
-      win.focus();
-  }
-  return false;
-  } // end of function OpenWindow
+	var win = window.open(url, winname, spec);
+	if (true == focus) {
+		win.focus();
+	}
+	return false;
+} // end of func OpenWindow
 
 
 /**
  * 接受参数，并将内容写入指定对象的指定区域
+ *
  * @param   string  id      目标对象的ID
  * @param   string  part    目标对象的子区域
  * @param   string  data    要写入的数据
  */
-function SetData(id, part, data)
-{
-    if ( 'value' == part )
-    {
+function SetData (id, part, data) {
+    if ('value' == part) {
         GetElement(document, id).value = data;
     }
-    if ( 'innerHTML' == part )
-    {
+    if ('innerHTML' == part) {
         GetElement(document, id).innerHTML = data;
     }
-	if ( 'src' == part )
-	{
+	if ('src' == part) {
 		GetElement(document, id).src = data;
 	}
-    if ( 'text' == part )
-    {
+    if ('text' == part) {
         GetElement(document, id).text = data;
     }
-} // end of function SetData
+} // end of func SetData
