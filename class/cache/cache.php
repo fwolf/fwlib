e80b95cf1796b9d26d77b3402c86fd6d647fa4e3
Fwolf <fwolf.aide+git@gmail.com>
2012-11-13 11:18:59 +0800

    Add: Cache val encode/decode type define and method.

--- class/cache/cache.php
+++ class/cache/cache.php
@@ -1,11 +1,14 @@
 <?php
 require_once(dirname(__FILE__) . '/../fwolflib.php');
 require_once(dirname(__FILE__) . '/../../func/array.php');
+require_once(dirname(__FILE__) . '/../../func/string.php');
 
 
 /**
- * Parent class for Key - value cache system,
- * data store in various way.
+ * Parent class for Key - value cache system
+ *
+ * Data store in various way, define in sub class, call use Create().
+ * As factory This class is also subclass creator, so not abstract.
  *
  * Main method:
  * -	Key(), hash or use original key.
@@ -13,7 +16,10 @@ require_once(dirname(__FILE__) . '/../../func/array.php');
  * -	Get(), read cache data
  * -	Del(), delete cache data
  *
- * Parent class is also subclass creator, so not abstract.
+ * Cache store method/flag:
+ * 0: Raw string or other value type suite for cache type.
+ * 1: Json, decode to array.
+ * 2: Json, decode to object.
  *
  * @package		fwolflib
  * @subpackage	class.cache
@@ -37,7 +43,7 @@ class Cache extends Fwolflib {
 	 * @param	array	$ar_cfg
 	 */
 	public function __construct ($ar_cfg = array()) {
-		parent::__construct();
+		parent::__construct($ar_cfg);
 
 		// Unset for auto new
 	} // end of func __construct
@@ -46,7 +52,7 @@ class Cache extends Fwolflib {
 	/**
 	 * Factory create method
 	 *
-	 * @param	string	$type		Cache type
+	 * @param	string	$type			Cache type
 	 * @param	array	$ar_cfg
 	 * @return	object
 	 */
@@ -87,13 +93,31 @@ class Cache extends Fwolflib {
 
 
 	/**
+	 * Is cache data expire ?
+	 *
+	 * @param	string	$key
+	 * @return	boolean
+	 */
+	public function Expire ($key) {
+		// Inner var never expire,
+		// Also, there is no good method to keep var set time.
+		return false;
+	} // end of func Expire
+
+
+	/**
 	 * Load cache data
 	 *
 	 * @param	string	$key
+	 * @param	int		$flag			Cache store method
+	 * @param	int		$lifetime		Cache lifetime
 	 * @return	mixed
 	 */
-	public function Get ($key) {
-		return ArrayRead($this->aCache, $this->Key($key));
+	public function Get ($key, $flag = 0, $lifetime = 0) {
+		// Ignored flag and lifetime
+		return $this->ValDecode(
+			ArrayRead($this->aCache, $this->Key($key))
+			, 0);
 	} // end of func Get
 
 
@@ -115,10 +139,13 @@ class Cache extends Fwolflib {
 	 *
 	 * @param	string	$key
 	 * @param	mixed	$val
+	 * @param	int		$flag			Cache store method
+	 * @param	int		$lifetime
 	 * @return	$this
 	 */
-	public function Set ($key, $val) {
-		$this->aCache[$this->Key($key)] = $val;
+	public function Set ($key, $val, $flag = 0, $lifetime = 0) {
+		// Flag is ignored, lifetime is useless.
+		$this->aCache[$this->Key($key)] = $this->ValEncode($val, 0);
 		return $this;
 	} // end of func Set
 
@@ -138,6 +165,51 @@ class Cache extends Fwolflib {
 
 		return $this;
 	} // end of func SetCfgDefault
+
+
+	/**
+	 * Decode val stored in cache
+	 *
+	 * Lifetime get/set various by cache type, assign in subclass
+	 *
+	 * @param	string	$str			Str read from cache
+	 * @param	int		$flag			Cache store method
+	 * @return	mixed
+	 */
+	public function ValDecode ($str, $flag = 0) {
+		if (1 == $flag) {
+			// Json to array
+			return json_decode($str, true);
+		}
+		elseif (2 == $flag) {
+			// Json to object
+			return json_decode($str, false);
+		}
+		else {
+			// Flag = 0 or other, return raw.
+			return $str;
+		}
+	} // end of func ValDecode
+
+
+	/**
+	 * Encode val to store in cache
+	 *
+	 * Lifetime get/set various by cache type, assign in subclass
+	 *
+	 * @param	mixed	$val
+	 * @param	int		$flag			Cache store method
+	 * @return	string
+	 */
+	public function ValEncode ($val, $flag = 0) {
+		if (1 == $flag || 2 == $flag) {
+			return JsonEncodeUnicode($val);
+		}
+		else {
+			// Raw
+			return $val;
+		}
+	} // end of func ValEncode
 
 
 } // end of class Cache
