611acf69b6860c0965502e85ae5e92b09ebb645a
Fwolf <fwolf.aide+git@gmail.com>
2012-11-13 14:00:17 +0800

    New: class CacheFile and test case.

---
+++ class/cache/cache-file.php
@@ -0,0 +1,317 @@
+<?php
+require_once(dirname(__FILE__) . '/cache.php');
+require_once(FWOLFLIB . 'func/filesystem.php');
+
+
+/**
+ * Key - value cache system, data store in file.
+ *
+ * Key is split by '/', just like URL.
+ *
+ * @package		fwolflib
+ * @subpackage	class.cache
+ * @copyright	Copyright 2010-2012, Fwolf
+ * @author		Fwolf <fwolf.aide+fwolflib.class.cache@gmail.com>
+ * @since		2010-01-07
+ */
+class CacheFile extends Cache {
+
+
+	/**
+	 * Constructor
+	 *
+	 * @param	array	$ar_cfg
+	 */
+	public function __construct ($ar_cfg = array()) {
+		parent::__construct($ar_cfg);
+
+		// Unset for auto new
+	} // end of func __construct
+
+
+	/**
+	 * Check if cache is ready for use.
+	 *
+	 * @return	boolean
+	 */
+	public function ChkCfg () {
+		$b_pass = true;
+
+		if (empty($this->aCfg['cache-file-dir']))
+			$b_pass = false;
+		else {
+			$s = $this->ChkCfgFileDir($this->aCfg['cache-file-dir']);
+			if (!empty($s)) {
+				$this->Log('Cache file cfg dir error: ' . $s, 4);
+				$b_pass = false;
+			}
+		}
+
+		if (empty($this->aCfg['cache-file-rule']))
+			$b_pass = false;
+		else {
+			$s = $this->ChkCfgFileRule($this->aCfg['cache-file-rule']);
+			if (!empty($s)) {
+				$this->Log('Cache file cfg rule error: ' . $s, 4);
+				$b_pass = false;
+			}
+		}
+
+		return $b_pass;
+	} // end of func ChkCfg
+
+
+	/**
+	 * Check config/cache store dir valid and writable
+	 * If error, return error msg, else return empty str.
+	 *
+	 * @param	string	$dir
+	 * @return	string
+	 */
+	public function ChkCfgFileDir ($dir) {
+		$s = '';
+
+		if (empty($dir))
+			$s = "Cache dir {$dir} is not defined.";
+
+		if (!file_exists($dir)) {
+			if (false == mkdir($dir, 0755, true))
+				$s = "Fail to create cache dir {$dir}.";
+		}
+		else {
+			if (!is_writable($dir))
+				$s = "Cache dir {$dir} is not writable.";
+		}
+
+		return $s;
+	} // end of func ChkCfgFileDir
+
+
+	/**
+	 * Check cache rule exist and valid
+	 * If error, return error msg, else return empty str.
+	 *
+	 * @param	string	$rule
+	 * @return	string
+	 */
+	public function ChkCfgFileRule($rule) {
+		if (2 > strlen($rule))
+			return("Cache rule is not defined or too short.");
+
+		if (0 != (strlen($rule) % 2))
+			return("Cache rule {$this->aCfg['cache-file-rule']} may not right.");
+
+		return '';
+	} // end of func ChkCfgFileRule
+
+
+	/**
+	 * Is cache data expire ?
+	 *
+	 * File cache does not keep lifetime in cache,
+	 * so it need a lifetime from outside,
+	 * or use default lifetime config.
+	 *
+	 * @param	string	$key
+	 * @param	int		$lifetime	Cache lifetime, in second.
+	 * @return	boolean				True means it IS expired.
+	 */
+	public function Expire ($key, $lifetime = 0) {
+		$s_file = $this->FilePath($key);
+
+		if (0 == $lifetime && 0!= $this->aCfg['cache-file-lifetime'])
+			$lifetime = $this->aCfg['cache-file-lifetime'];
+
+		// File doesn't exist
+		if (!file_exists($s_file))
+			return true;
+
+		// Out of expire time
+		if (time() > (filemtime($s_file) + $lifetime))
+			return true;
+		else
+			return false;
+	} // end of func Expire
+
+
+	/**
+	 * Compute path of a key's data file
+	 *
+	 * @param	string	$key
+	 * @return	string
+	 */
+	public function FilePath ($key) {
+		$s_path = $this->aCfg['cache-file-dir'];
+
+		$ar_rule = str_split($this->aCfg['cache-file-rule'], 2);
+		if (empty($ar_rule))
+			return $s_path;
+
+		foreach ($ar_rule as $rule)
+			$s_path .= $this->FilePathSec($rule, $key) . '/';
+
+		// Filename
+		$s_path .= $this->FilePathFilename($key);
+
+		return $s_path;
+	} // end of func FilePath
+
+
+	/**
+	 * Compute name of a key's data file
+	 *
+	 * @param	string	$key
+	 * @return	string
+	 */
+	protected function FilePathFilename($key) {
+		return substr(md5($key), 0, 8);
+	} // end of func FilePathFilename
+
+
+	/**
+	 * Compute path of a key by a single rule section
+	 *
+	 * @param	string	$rule
+	 * @param	string	$key
+	 * @return	string
+	 * @see	$sCacheRule
+	 */
+	protected function FilePathSec($rule, $key) {
+		$i_len = 2;
+
+		if ($i_len > strlen($rule))
+			return '';
+
+		$i = intval($rule{1});
+		if (1 == $rule{0}) {
+			// md5 from start
+			$i_start = $i_len * $i;
+			$s_seed = md5($key);
+		} elseif (2 == $rule{0}) {
+			// md5 from end
+			$i_start = -1 * $i_len * ($i + 1);
+			$s_seed = md5($key);
+		} elseif (3 == $rule{0}) {
+			// raw from start
+			$i_start = $i_len * $i;
+			$s_seed = $key;
+		} elseif (4 == $rule{0}) {
+			// raw from end
+			$i_start = -1 * $i_len * ($i + 1);
+			$s_seed = $key;
+		} elseif (5 == $rule{0}) {
+			// crc32
+			if (3 < $i)
+				$i = $i % 3;
+			$i_start = $i_len * $i;
+			$s_seed = hash('crc32', $key);
+		}
+		return(substr($s_seed, $i_start, 2));
+	} // end of func FilePathSec
+
+
+	/**
+	 * Read cache and return value
+	 *
+	 * File cache should check lifetime when get,
+	 * return NULL when fail.
+	 *
+	 * @param	string	$key
+	 * @param	int		$flag			Cache store method
+	 * @param	boolean	$b_chk_lt		Check lifetime or not
+	 * @param	int		$lifetime		Cache lifetime
+	 * @return	mixed
+	 */
+	public function Get ($key, $flag = 0, $b_chk_lt = true, $lifetime = 0) {
+		if ($b_chk_lt && $this->Expire($key, $lifetime)) {
+				return NULL;
+		}
+
+		// Read from file and parse it.
+		$s_file = $this->FilePath($key);
+		$s_cache = file_get_contents($s_file);
+
+		return $this->ValDecode($s_cache, $flag);
+	} // end of func CacheGetFile
+
+
+	/**
+	 * Init treatment
+	 *
+	 * @param	array	$ar_cfg
+	 * @return	object
+	 */
+	public function Init () {
+		parent::Init();
+
+		$this->ChkCfg();
+
+		return $this;
+	} // end of func Init
+
+
+	/**
+	 * Write data to cache
+	 *
+	 * Lifetime check when get.
+	 *
+	 * @param	string	$key
+	 * @param	mixed	$val
+	 * @param	int		$flag			Cache store method
+	 * @return	$this
+	 */
+	public function Set ($key, $val, $flag = 0) {
+		$s_file = $this->FilePath($key);
+		$s_cache = $this->ValEncode($val, $flag);
+
+		// Create each level dir if not exists
+		$s_dir = DirName1($s_file);
+		if (!file_exists($s_dir))
+			mkdir($s_dir, 0755, true);
+
+		// Finally write file
+		file_put_contents($s_file, $s_cache, LOCK_EX);
+
+		return $this;
+	} // end of func Set
+
+
+	/**
+	 * Set default config
+	 *
+	 * @return	this
+	 */
+	protected function SetCfgDefault () {
+		parent::SetCfgDefault();
+
+		// Cache type: file
+		$this->aCfg['cache-type'] = 'file';
+
+
+		// Dir where data file store
+		$this->aCfg['cache-file-dir'] = '/tmp/cache/';
+
+		/**
+		 * Cache file store rule
+		 *
+		 * Group by every 2-chars, their means:
+		 * 10	first 2 char of md5 hash, 16 * 16 = 256
+		 * 11	3-4 char of md5 hash
+		 * 20	last 2 char of md5 hash
+		 * 30	first 2 char of key
+		 * 40	last 2 char of key
+		 * 5n	crc32, n=0..3, 16 * 16 = 256
+		 * Join these str with '/', got full path of cache file.
+		 */
+		$this->aCfg['cache-file-rule'] = '10';
+
+		// Default cache lifetime, 60s * 60m * 24h = 86400s(1d)
+		$this->aCfg['cache-file-lifetime'] = 86400;
+
+
+		return $this;
+	} // end of func SetCfgDefault
+
+
+} // end of class CacheFile
+?>
---
+++ class/cache/cache.test.php
@@ -0,0 +1,178 @@
+<?php
+/**
+ * Test - Cache class
+ * @package     fwolflib
+ * @subpackage	class.test
+ * @copyright   Copyright 2012, Fwolf
+ * @author      Fwolf <fwolf.aide+fwolflib.class.test.cache@gmail.com>
+ * @since		2012-11-06
+ */
+
+// Define like this, so test can run both under eclipse and web alone.
+// {{{
+if (! defined('SIMPLE_TEST')) {
+	define('SIMPLE_TEST', 'simpletest/');
+	require_once(SIMPLE_TEST . 'autorun.php');
+}
+// Then set output encoding
+//header('Content-Type: text/html; charset=utf-8');
+// }}}
+
+// Require library define file which need test
+require_once(dirname(__FILE__) . '/../fwolflib.php');
+require_once(FWOLFLIB . 'class/cache/cache.php');
+require_once(FWOLFLIB . 'func/ecl.php');
+require_once(FWOLFLIB . 'func/request.php');
+require_once(FWOLFLIB . 'func/string.php');
+
+class TestCache extends UnitTestCase {
+
+	/**
+	 * Cache object
+	 * @var	object
+	 */
+	protected $oCh = null;
+
+
+	/**
+	 * Constructor
+	 */
+	public function __construct () {
+
+	} // end of func __construct
+
+
+	public function TestCacheDefault () {
+		//$this->oCh = new Cache();
+		//$this->oCh = Cache::Create('unsupported');
+		//$this->assertEqual(NULL, $this->oCh);
+
+		$this->oCh = Cache::Create('');
+		$this->assertEqual('', $this->oCh->aCfg['cache-type']);
+
+		$key = 'key';
+		$val = 'val';
+		$this->oCh->Set($key, $val);
+		$this->assertEqual($val, $this->oCh->Get($key));
+
+		$this->oCh->Del($key);
+		$this->assertEqual(NULL, $this->oCh->Get($key));
+
+
+		// Val encode and decode
+		$x = 'This is string.';
+		$this->assertEqual($x, $this->oCh->ValDecode(
+			$this->oCh->ValEncode($x, 0), 0));
+		// Invalid flag
+		$this->assertEqual($x, $this->oCh->ValDecode(
+			$this->oCh->ValEncode($x, 100), 100));
+
+		$x = array('a' => 'b');
+		$this->assertEqual($x, $this->oCh->ValDecode(
+			$this->oCh->ValEncode($x, 0), 0));
+		$this->assertEqual($x, $this->oCh->ValDecode(
+			$this->oCh->ValEncode($x, 1), 1));
+
+		$x = new Fwolflib;
+		$this->assertEqual($x, $this->oCh->ValDecode(
+			$this->oCh->ValEncode($x, 0), 0));
+/*
+		// Json decode object has no class, so diff with original
+		// Also array in object must be convert back from stdClass
+		$y = $this->oCh->ValDecode($this->oCh->ValEncode($x, 2), 2);
+		$y->aCfg = (array)$y->aCfg;
+		$this->assertEqual($x, $y);
+		var_dump($x);
+		var_dump($y);
+*/
+
+	} // end of func TestCacheDefault
+
+
+	public function TestCacheFile () {
+/*
+		// Wrong cfg test
+		$this->oCh = Cache::Create('file'
+			, array('cache-file-dir' => '/proc/'));
+		$this->assertEqual(false, $this->oCh->ChkCfg());
+
+		$this->oCh = Cache::Create('file'
+			, array('cache-file-rule' => '0blah'));
+		$this->assertEqual(false, $this->oCh->ChkCfg());
+*/
+
+		$this->oCh = Cache::Create('file');
+		$this->oCh->SetCfg('cache-file-rule', '1140');
+		$key = 'site/index';
+
+		$x = '/tmp/cache/d0/ex/3ed0dc6e';
+		$y = $this->oCh->FilePath($key);
+		$this->assertEqual($x, $y);
+
+		$this->oCh->SetCfg(array('cache-file-rule' => '1131'));
+		$x = '/tmp/cache/d0/te/3ed0dc6e';
+		$y = $this->oCh->FilePath($key);
+		$this->assertEqual($x, $y);
+
+		// Notice: Directly use key's part as path may cause wrong
+		$this->oCh->SetCfg(array('cache-file-rule' => '2342'));
+		$x = '/tmp/cache/57//i/3ed0dc6e';
+		$y = $this->oCh->FilePath($key);
+		$this->assertEqual($x, $y);
+
+		// Common usage
+		$this->oCh->SetCfg(array('cache-file-rule' => '1011'));
+		$x = '/tmp/cache/3e/d0/3ed0dc6e';
+		$y = $this->oCh->FilePath($key);
+		$this->assertEqual($x, $y);
+
+		// Common usage 2
+		$this->oCh->SetCfg(array('cache-file-rule' => '2021'));
+		$x = '/tmp/cache/b6/9c/3ed0dc6e';
+		$y = $this->oCh->FilePath($key);
+		$this->assertEqual($x, $y);
+
+		// Common usage 3
+		$this->oCh->SetCfg(array('cache-file-rule' => '55'));
+		$x = '/tmp/cache/89/3ed0dc6e';
+		$y = $this->oCh->FilePath($key);
+		$this->assertEqual($x, $y);
+
+
+		// Skip cache write after test.
+/*
+		// Cache set
+		$v = 'blah';
+		$this->oCh->Set($key, $v, 1);
+		$this->assertEqual(json_encode($v), file_get_contents($x));
+
+		// Cache expire
+		$this->assertEqual(true, $this->oCh->Expire($key, -10));
+		$this->assertEqual(false, $this->oCh->Expire($key, 10));
+		$this->assertEqual(false, $this->oCh->Expire($key, 0));
+
+		// Cache get
+		$this->assertEqual($v, $this->oCh->Get($key, 1));
+
+		$v = '你好';
+		$this->oCh->Set($key, $v, 0);
+		$this->assertEqual($v, $this->oCh->Get($key, 0));
+
+		$v = array('你' => '好');
+		$this->oCh->Set($key, $v, 1);
+		$this->assertEqual($v, $this->oCh->Get($key, 1));
+*/
+
+	} // end of func TestCacheFile
+
+
+// Change output charset in this way.
+// {{{
+$s_url = GetSelfUrl(false);
+$s_url = substr($s_url, strrpos($s_url, '/') + 1);
+if ('cache.test.php' == $s_url) {
+	$test = new TestCache();
+	$test->run(new HtmlReporter('utf-8'));
+}
+// }}}
+?>
