f3361b167b4bf4a6cda934d09f918812458f5bdb
Fwolf <fwolf.aide+git@gmail.com>
2013-07-30 17:02:49 +0800

    Move testcase to FwlibTest namespace

--- FwlibTest/Base/ClassLoaderTest.php
+++ FwlibTest/Base/ClassLoaderTest.php
@@ -1,10 +1,10 @@
 <?php
 require __DIR__ . '/../../autoload.php';
-//$loader->addPrefix('Fwlib\\Base', __DIR__ . '/../../');
+$loader->addPrefix('FwlibTest\\Base', __DIR__ . '/../../');
 $loader->addPrefix('Foo1', __DIR__ . '/Foo1.php');
 
 $foo = new Foo1;
 
-use Fwlib\Base\Foo;
+use FwlibTest\Base\Foo;
 
 $foo = new Foo;
