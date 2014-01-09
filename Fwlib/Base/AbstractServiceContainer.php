d7cd6bc7ab3377ec98af62868d440cb4c4a70c61
Fwolf <fwolf.aide+git@gmail.com>
2014-01-09 22:33:15 +0800

    Base: New ServiceContainerInterface

--- Fwlib/Base/AbstractServiceContainer.php
+++ Fwlib/Base/AbstractServiceContainer.php
@@ -2,42 +2,21 @@
 namespace Fwlib\Base;
 
 use Fwlib\Base\AbstractSingleton;
+use Fwlib\Base\ServiceContainerInterface;
 
 /**
  * Service Container
  *
- * For avoid of multiple create instance of some common class, such as Db,
- * Cache, make them as service object and store here to use in variant place.
- *
- * Use Dependency Injection on constructor is good but not conveniency, create
- * Factory for each class need inject is also complicate, so ServiceContainer
- * can be substitution. If dependent object is not passed in when construct,
- * using these object will trigger newInstanceXxx() method in
- * AbstractAutoNewInstance class, here we can use ServiceContainer to provide
- * real object instance, the only work needed is do setServiceContainer()
- * once. Another similar way is doing outside of the class, create instance by
- * ServiceContainer or other, then use setInstance() method to inject.
- *
- * In short, if Dependency Injection is skipped, get ServiceContainer instance
- * and assign use setter, you will got worked dependent object automatic,
- * without loose visual dependent relationship.
- *
- * In this way, test or inherit ServiceContainer is easy too, just assign
- * another container instance with setter.
- *
  * Usually only one container instance is need, so use Singleton pattern.
  *
- * When create class instance(Service) in container, it's also better to use
- * Dependency Injection: create dependent object and pass to constructor. If
- * necessary, assign ServiceContainer instance here for later usage.
- *
  * @package     Fwlib\Base
- * @copyright   Copyright 2013 Fwolf
+ * @copyright   Copyright 2013-2014 Fwolf
  * @author      Fwolf <fwolf.aide+Fwlib@gmail.com>
  * @license     http://www.gnu.org/licenses/lgpl.html LGPL v3
  * @since       2013-11-07
  */
-abstract class AbstractServiceContainer extends AbstractSingleton
+abstract class AbstractServiceContainer extends AbstractSingleton implements
+    ServiceContainerInterface
 {
     /**
      * Class name for call Class::getInstance()
@@ -55,11 +34,7 @@ abstract class AbstractServiceContainer extends AbstractSingleton
 
 
     /**
-     * Get service by name
-     *
-     * When $forcenew is true, it will ignore exists service and create a new
-     * one-time use service object, which will not be stored in instance
-     * array.
+     * {@inheritdoc}
      *
      * $forcenew doesn't affect child class of Fwlib\Base\AbstractSingleton.
      *
@@ -122,11 +97,11 @@ abstract class AbstractServiceContainer extends AbstractSingleton
 
 
     /**
-     * Register service class or instance
+     * {@inheritdoc}
      *
      * @param   string  $name
      * @param   string|object   $service
-     * @return  $this
+     * @return  AbstractServiceContainer
      */
     public function register($name, $service)
     {
@@ -139,15 +114,11 @@ abstract class AbstractServiceContainer extends AbstractSingleton
 
 
     /**
-     * Register service class
-     *
-     * For class implement Fwlib\Base\AbstractSingleton or similar, can be
-     * instanced simplely by find class name in serviceClass array and call
-     * its getInstance() method, avoid define of newServiceXxx() method.
+     * {@inheritdoc}
      *
      * @param   string  $name
      * @param   string  $className  Full qualified name without leading '\'
-     * @return  $this
+     * @return  AbstractServiceContainer
      */
     public function registerClass($name, $className)
     {
@@ -158,13 +129,11 @@ abstract class AbstractServiceContainer extends AbstractSingleton
 
 
     /**
-     * Register service instance
-     *
-     * Registered instance can directly use without newServiceXxx() method.
+     * {@inheritdoc}
      *
      * @param   string  $name
      * @param   object  $instance
-     * @return  $this
+     * @return  AbstractServiceContainer
      */
     public function registerInstance($name, $instance)
     {
---
+++ Fwlib/Base/ServiceContainerInterface.php
@@ -0,0 +1,87 @@
+<?php
+namespace Fwlib\Base;
+
+
+/**
+ * Service Container interface
+ *
+ * For avoid of multiple create instance of some common class, such as Db,
+ * Cache, make them as service object and store here to use in variant place.
+ *
+ * Use Dependency Injection on constructor is good but not conveniency, create
+ * Factory for each class need inject is also complicate, so ServiceContainer
+ * can be substitution. If dependent object is not passed in when construct,
+ * using these object will trigger newInstanceXxx() method in
+ * AbstractAutoNewInstance class, here we can use ServiceContainer to provide
+ * real object instance, the only work needed is do setServiceContainer()
+ * once. Another similar way is doing outside of the class, create instance by
+ * ServiceContainer or other, then use setInstance() method to inject.
+ *
+ * In short, if Dependency Injection is skipped, get ServiceContainer instance
+ * and assign use setter, you will got worked dependent object automatic,
+ * without loose visual dependent relationship.
+ *
+ * In this way, test or inherit ServiceContainer is easy too, just assign
+ * another container instance with setter.
+ *
+ * When create class instance(Service) in container, it's also better to use
+ * Dependency Injection: create dependent object and pass to constructor. If
+ * necessary, assign ServiceContainer instance here for later usage.
+ *
+ * @package     Fwlib\Base
+ * @copyright   Copyright 2014 Fwolf
+ * @author      Fwolf <fwolf.aide+Fwlib@gmail.com>
+ * @license     http://www.gnu.org/licenses/lgpl.html LGPL v3
+ * @since       2014-01-09
+ */
+interface ServiceContainerInterface
+{
+    /**
+     * Get service instance by name
+     *
+     * When $forcenew is true, it will ignore exists service and create a new
+     * one-time use service object, which will not be stored in instance
+     * array.
+     *
+     * @param   string  $name
+     * @param   boolean $forcenew
+     * @return  object
+     */
+    public function get($name, $forcenew = false);
+
+
+    /**
+     * Register service class or instance
+     *
+     * @param   string  $name
+     * @param   string|object   $service
+     * @return  ServiceContainerInterface
+     */
+    public function register($name, $service);
+
+
+    /**
+     * Register service class
+     *
+     * For class implement Fwlib\Base\AbstractSingleton or similar, can be
+     * instanced simplely by find class name in serviceClass array and call
+     * its getInstance() method, avoid define of newServiceXxx() method.
+     *
+     * @param   string  $name
+     * @param   string  $className  Full qualified name without leading '\'
+     * @return  ServiceContainerInterface
+     */
+    public function registerClass($name, $className);
+
+
+    /**
+     * Register service instance
+     *
+     * Registered instance can directly use without newServiceXxx() method.
+     *
+     * @param   string  $name
+     * @param   object  $instance
+     * @return  ServiceContainerInterface
+     */
+    public function registerInstance($name, $instance);
+}
