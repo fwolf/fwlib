31d64c017d2fcfd92392c8bea90e9f8d111d88b4
Fwolf <fwolf.aide+git@gmail.com>
2014-11-20 01:56:59 +0800

    Util: Add individual getter method for each util
    By call these getter methods, the IDE can recoginize correct return type.

--- Fwlib/Util/Test/UtilContainerTest.php
+++ Fwlib/Util/Test/UtilContainerTest.php
@@ -6,9 +6,7 @@ use Fwlib\Util\UtilContainer;
 
 /**
  * @copyright   Copyright 2013-2014 Fwolf
- * @author      Fwolf <fwolf.aide+Fwlib@gmail.com>
  * @license     http://www.gnu.org/licenses/lgpl.html LGPL v3
- * @since       2013-12-18
  */
 class UtilContainerTest extends PHPunitTestCase
 {
@@ -27,8 +25,101 @@ class UtilContainerTest extends PHPunitTestCase
 
     public function testGet()
     {
-        $util = $this->buildMock();
+        $utilContainer = $this->buildMock();
 
-        $this->assertEquals(42, $util->get('Array')->getIdx(array(), 'foo', 42));
+        $this->assertEquals(
+            42,
+            $utilContainer->get('Array')->getIdx(array(), 'foo', 42)
+        );
+
+        $this->assertInstanceOf(
+            'Fwlib\Util\ArrayUtil',
+            $utilContainer->getArray()
+        );
+
+        $this->assertInstanceOf(
+            'Fwlib\Util\DatetimeUtil',
+            $utilContainer->getDatetime()
+        );
+
+        $this->assertInstanceOf(
+            'Fwlib\Util\Env',
+            $utilContainer->getEnv()
+        );
+
+        $this->assertInstanceOf(
+            'Fwlib\Util\EscapeColor',
+            $utilContainer->getEscapeColor()
+        );
+
+        $this->assertInstanceOf(
+            'Fwlib\Util\FileSystem',
+            $utilContainer->getFileSystem()
+        );
+
+        $this->assertInstanceOf(
+            'Fwlib\Util\HttpUtil',
+            $utilContainer->getHttp()
+        );
+
+        $this->assertInstanceOf(
+            'Fwlib\Util\Ip',
+            $utilContainer->getIp()
+        );
+
+        $this->assertInstanceOf(
+            'Fwlib\Util\Json',
+            $utilContainer->getJson()
+        );
+
+        $this->assertInstanceOf(
+            'Fwlib\Util\McryptSimpleIv',
+            $utilContainer->getMcryptSimpleIv()
+        );
+
+        $this->assertInstanceOf(
+            'Fwlib\Util\NumberUtil',
+            $utilContainer->getNumber()
+        );
+
+        $this->assertInstanceOf(
+            'Fwlib\Util\Rfc2047',
+            $utilContainer->getRfc2047()
+        );
+
+        $this->assertInstanceOf(
+            'Fwlib\Util\StringUtil',
+            $utilContainer->getString()
+        );
+
+        $this->assertInstanceOf(
+            'Fwlib\Util\UuidBase16',
+            $utilContainer->getUuidBase16()
+        );
+
+        $this->assertInstanceOf(
+            'Fwlib\Util\UuidBase36',
+            $utilContainer->getUuidBase36()
+        );
+
+        $this->assertInstanceOf(
+            'Fwlib\Util\UuidBase62',
+            $utilContainer->getUuidBase62()
+        );
+
+        $this->assertInstanceOf(
+            'Fwlib\Util\Algorithm\Iso7064',
+            $utilContainer->getIso7064()
+        );
+
+        $this->assertInstanceOf(
+            'Fwlib\Util\Code\ChnCitizenIdentificationNumber',
+            $utilContainer->getChnCin()
+        );
+
+        $this->assertInstanceOf(
+            'Fwlib\Util\Code\ChnOrganizationCode',
+            $utilContainer->getChnOrganizationCode()
+        );
     }
 }
--- Fwlib/Util/UtilContainer.php
+++ Fwlib/Util/UtilContainer.php
@@ -8,9 +8,7 @@ use Fwlib\Util\UtilContainerInterface;
  * Util class container
  *
  * @copyright   Copyright 2013-2014 Fwolf
- * @author      Fwolf <fwolf.aide+Fwlib@gmail.com>
  * @license     http://www.gnu.org/licenses/lgpl.html LGPL v3
- * @since       2013-12-18
  */
 class UtilContainer extends AbstractServiceContainer implements
     UtilContainerInterface
@@ -38,7 +36,7 @@ class UtilContainer extends AbstractServiceContainer implements
         'Number'            => 'Fwlib\Util\NumberUtil',
         'NumberUtil'        => 'Fwlib\Util\NumberUtil',
         'Rfc2047'           => 'Fwlib\Util\Rfc2047',
-        'String'            => 'Fwlib\Util\String',
+        'String'            => 'Fwlib\Util\StringUtil',
         'StringUtil'        => 'Fwlib\Util\StringUtil',
         'UuidBase16'        => 'Fwlib\Util\UuidBase16',
         'UuidBase36'        => 'Fwlib\Util\UuidBase36',
@@ -48,9 +46,171 @@ class UtilContainer extends AbstractServiceContainer implements
 
         'ChnCin'                => 'Fwlib\Util\Code\ChnCitizenIdentificationNumber',
         'ChnCinCode'            => 'Fwlib\Util\Code\ChnCitizenIdentificationNumber',
-        'ChnOrgCode'            => 'Fwlib\Util\Code\ChnOrgCode',
-        'ChnOrganizationCode'   => 'Fwlib\Util\Code\ChnOrgCode',
+        'ChnOrgCode'            => 'Fwlib\Util\Code\ChnOrganizationCode',
+        'ChnOrganizationCode'   => 'Fwlib\Util\Code\ChnOrganizationCode',
     );
+
+
+    /**
+     * @return  \Fwlib\Util\ArrayUtil
+     */
+    public function getArray()
+    {
+        return $this->get('ArrayUtil');
+    }
+
+
+    /**
+     * @return  \Fwlib\Util\DatetimeUtil
+     */
+    public function getDatetime()
+    {
+        return $this->get('DatetimeUtil');
+    }
+
+
+    /**
+     * @return  \Fwlib\Util\Env
+     */
+    public function getEnv()
+    {
+        return $this->get('Env');
+    }
+
+
+    /**
+     * @return  \Fwlib\Util\EscapeColor
+     */
+    public function getEscapeColor()
+    {
+        return $this->get('EscapeColor');
+    }
+
+
+    /**
+     * @return  \Fwlib\Util\FileSystem
+     */
+    public function getFileSystem()
+    {
+        return $this->get('FileSystem');
+    }
+
+
+    /**
+     * @return  \Fwlib\Util\HttpUtil
+     */
+    public function getHttp()
+    {
+        return $this->get('HttpUtil');
+    }
+
+
+    /**
+     * @return  \Fwlib\Util\Ip
+     */
+    public function getIp()
+    {
+        return $this->get('Ip');
+    }
+
+
+    /**
+     * @return  \Fwlib\Util\Json
+     */
+    public function getJson()
+    {
+        return $this->get('Json');
+    }
+
+
+    /**
+     * @return  \Fwlib\Util\McryptSimpleIv
+     */
+    public function getMcryptSimpleIv()
+    {
+        return $this->get('McryptSimpleIv');
+    }
+
+
+    /**
+     * @return  \Fwlib\Util\NumberUtil
+     */
+    public function getNumber()
+    {
+        return $this->get('NumberUtil');
+    }
+
+
+    /**
+     * @return  \Fwlib\Util\Rfc2047
+     */
+    public function getRfc2047()
+    {
+        return $this->get('Rfc2047');
+    }
+
+
+    /**
+     * @return  \Fwlib\Util\StringUtil
+     */
+    public function getString()
+    {
+        return $this->get('StringUtil');
+    }
+
+
+    /**
+     * @return  \Fwlib\Util\UuidBase16
+     */
+    public function getUuidBase16()
+    {
+        return $this->get('UuidBase16');
+    }
+
+
+    /**
+     * @return  \Fwlib\Util\UuidBase36
+     */
+    public function getUuidBase36()
+    {
+        return $this->get('UuidBase36');
+    }
+
+
+    /**
+     * @return  \Fwlib\Util\UuidBase62
+     */
+    public function getUuidBase62()
+    {
+        return $this->get('UuidBase62');
+    }
+
+
+    /**
+     * @return  \Fwlib\Util\Algorithm\Iso7064
+     */
+    public function getIso7064()
+    {
+        return $this->get('Iso7064');
+    }
+
+
+    /**
+     * @return  \Fwlib\Util\Code\ChnCitizenIdentificationNumber
+     */
+    public function getChnCin()
+    {
+        return $this->get('ChnCin');
+    }
+
+
+    /**
+     * @return  \Fwlib\Util\Code\ChnOrganizationCode
+     */
+    public function getChnOrganizationCode()
+    {
+        return $this->get('ChnOrganizationCode');
+    }
 
 
     /**
