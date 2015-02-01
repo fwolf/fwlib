9daa1e6bddf266fcfa1febda43f7963e8dfe04ea
Fwolf <fwolf.aide+git@gmail.com>
2015-02-02 00:46:01 +0800

    Util: Use buildMock() instead of new instance in constructor

--- tests/FwlibTest/Util/StringUtilTest.php
+++ tests/FwlibTest/Util/StringUtilTest.php
@@ -3,6 +3,7 @@ namespace FwlibTest\Util;
 
 use Fwlib\Bridge\PHPUnitTestCase;
 use Fwlib\Util\StringUtil;
+use Fwlib\Util\UtilContainer;
 
 /**
  * @copyright   Copyright 2004-2015 Fwolf
@@ -11,29 +12,33 @@ use Fwlib\Util\StringUtil;
 class StringUtilTest extends PHPunitTestCase
 {
     public $dummyForTestJsonEncodeHex = 42;
-    public $stringUtil;
 
 
-    public function __construct()
+    /**
+     * @return  StringUtil
+     */
+    protected function buildMock()
     {
-        $this->stringUtil = new StringUtil;
+        return UtilContainer::getInstance()->getString();
     }
 
 
     public function testAddSlashesRecursive()
     {
+        $stringUtil = $this->buildMock();
+
         $x = '';
-        $y = $this->stringUtil->addSlashesRecursive($x);
+        $y = $stringUtil->addSlashesRecursive($x);
         $z = '';
         $this->assertEquals($y, $z);
 
         $x = 'it\'s ok';
-        $y = $this->stringUtil->addSlashesRecursive($x);
+        $y = $stringUtil->addSlashesRecursive($x);
         $z = 'it\\\'s ok';
         $this->assertEquals($y, $z);
 
         $x = array('it\'s ok');
-        $y = $this->stringUtil->addSlashesRecursive($x);
+        $y = $stringUtil->addSlashesRecursive($x);
         $z = array('it\\\'s ok');
         $this->assertEqualArray($y, $z);
 
@@ -59,67 +64,75 @@ class StringUtilTest extends PHPunitTestCase
             "end",
             ),
         );
-        $this->assertEquals($y, $this->stringUtil->addslashesRecursive($x));
+        $this->assertEquals($y, $stringUtil->addslashesRecursive($x));
 
         // Object, should return original
         $x = new StringUtilTest;
         $z = $x;
-        $y = $this->stringUtil->addSlashesRecursive($x);
+        $y = $stringUtil->addSlashesRecursive($x);
         $this->assertEquals($y, $z);
     }
 
 
     public function testEncodeHtml()
     {
+        $stringUtil = $this->buildMock();
+
         $x = '&';
         $y = '&amp;';
-        $this->assertEquals($y, $this->stringUtil->encodeHtml($x));
+        $this->assertEquals($y, $stringUtil->encodeHtml($x));
 
         $x = '<>';
         $y = '&lt;&gt;';
-        $this->assertEquals($y, $this->stringUtil->encodeHtml($x));
+        $this->assertEquals($y, $stringUtil->encodeHtml($x));
 
         $x = '  ';
         $y = '&nbsp; ';
-        $this->assertEquals($y, $this->stringUtil->encodeHtml($x));
+        $this->assertEquals($y, $stringUtil->encodeHtml($x));
 
         $x = ' ';
         $y = '&nbsp;';
-        $this->assertEquals($y, $this->stringUtil->encodeHtml($x));
+        $this->assertEquals($y, $stringUtil->encodeHtml($x));
 
         $x = '   ';
         $y = '&nbsp; &nbsp;';
-        $this->assertEquals($y, $this->stringUtil->encodeHtml($x));
+        $this->assertEquals($y, $stringUtil->encodeHtml($x));
 
         $x = '     ';
         $y = '&nbsp; &nbsp; &nbsp;';
-        $this->assertEquals($y, $this->stringUtil->encodeHtml($x));
+        $this->assertEquals($y, $stringUtil->encodeHtml($x));
 
         $x = "\r\n";
         $y = "<br />\r\n";
-        $this->assertEquals($y, $this->stringUtil->encodeHtml($x));
+        $this->assertEquals($y, $stringUtil->encodeHtml($x));
 
     }
 
 
     public function testEncodeHtmls()
     {
+        $stringUtil = $this->buildMock();
+
         $x = array('foo' => '&');
         $y = array('foo' => '&amp;');
-        $this->assertEquals($y, $this->stringUtil->encodeHtmls($x));
+        $this->assertEquals($y, $stringUtil->encodeHtmls($x));
     }
 
 
     public function testIndent()
     {
+        $stringUtil = $this->buildMock();
+
         $x = "  foo\n  bar";
         $y = "    foo\n    bar";
-        $this->assertEquals($y, $this->stringUtil->indent($x, 2));
+        $this->assertEquals($y, $stringUtil->indent($x, 2));
     }
 
 
     public function testIndentHtml()
     {
+        $stringUtil = $this->buildMock();
+
         $x = "  <textarea>
 foo
   bar
@@ -130,7 +143,7 @@ foo
   bar
 </textarea>
   <hr />";
-        $this->assertEquals($y, $this->stringUtil->indentHtml($x, 2));
+        $this->assertEquals($y, $stringUtil->indentHtml($x, 2));
 
 
         // Illegal string without ending tag
@@ -142,13 +155,13 @@ foo
 foo
   bar
 ";
-        $this->assertEquals($y, $this->stringUtil->indentHtml($x, 2));
+        $this->assertEquals($y, $stringUtil->indentHtml($x, 2));
     }
 
 
     public function testMatchWildcard()
     {
-        $stringUtil = $this->stringUtil;
+        $stringUtil = $this->buildMock();
 
         $this->assertTrue($stringUtil->matchWildcard('duck', '*c?'));
         $this->assertFalse($stringUtil->matchWildcard('duck', '?c*'));
@@ -162,43 +175,47 @@ foo
 
     public function testMatchRegex()
     {
+        $stringUtil = $this->buildMock();
+
         $x = 'The quick brown fox jumps over the lazy dog';
 
-        $y = $this->stringUtil->matchRegex('', $x);
+        $y = $stringUtil->matchRegex('', $x);
         $this->assertEquals(null, $y);
 
-        $y = $this->stringUtil->matchRegex('/\w{10}/', $x);
+        $y = $stringUtil->matchRegex('/\w{10}/', $x);
         $this->assertEquals(null, $y);
 
-        $y = $this->stringUtil->matchRegex('/\so\w{3}\s/', $x);
+        $y = $stringUtil->matchRegex('/\so\w{3}\s/', $x);
         $this->assertEquals(' over ', $y);
 
-        $y = $this->stringUtil->matchRegex('/\s(o\w{3})\s/', $x);
+        $y = $stringUtil->matchRegex('/\s(o\w{3})\s/', $x);
         $this->assertEquals('over', $y);
 
-        $y = $this->stringUtil->matchRegex('/\w{5}/', $x);
+        $y = $stringUtil->matchRegex('/\w{5}/', $x);
         $this->assertEqualArray(array('quick', 'brown', 'jumps'), $y);
 
-        $y = $this->stringUtil->matchRegex('/(\w{5})/', $x);
+        $y = $stringUtil->matchRegex('/(\w{5})/', $x);
         $this->assertEqualArray(array('quick', 'brown', 'jumps'), $y);
 
-        $y = $this->stringUtil->matchRegex('/((q\w+) (b\w+))/', $x);
+        $y = $stringUtil->matchRegex('/((q\w+) (b\w+))/', $x);
         $this->assertEqualArray(array('quick brown', 'quick', 'brown'), $y);
     }
 
 
     public function testRandom()
     {
-        $x = $this->stringUtil->random(10);
+        $stringUtil = $this->buildMock();
+
+        $x = $stringUtil->random(10);
         $this->assertEquals(10, strlen($x));
 
-        $x = $this->stringUtil->random(10, '0');
+        $x = $stringUtil->random(10, '0');
         $this->assertEquals('', preg_replace('/[0-9]/', '', $x));
 
-        $x = $this->stringUtil->random(10, 'a');
+        $x = $stringUtil->random(10, 'a');
         $this->assertEquals('', preg_replace('/[a-z]/', '', $x));
 
-        $x = $this->stringUtil->random(10, 'A');
+        $x = $stringUtil->random(10, 'A');
         $this->assertEquals('', preg_replace('/[A-Z]/', '', $x));
     }
 
@@ -206,104 +223,114 @@ foo
 
     public function testSubstrIgnoreHtml()
     {
+        $stringUtil = $this->buildMock();
+
         $x = '测试12&lt;4测试';
-        $x = $this->stringUtil->substrIgnoreHtml($x, 11, '...');
+        $x = $stringUtil->substrIgnoreHtml($x, 11, '...');
         $y = '测试12&lt;4...';
         $this->assertEquals($y, $x);
 
         $x = '测<b><i><br / >试</i></b>&quot;<b>234测试</b>';
-        $x = $this->stringUtil->substrIgnoreHtml($x, 9, '...');
+        $x = $stringUtil->substrIgnoreHtml($x, 9, '...');
         $y = '测<b><i><br / >试</i></b>&quot;<b>2...</b>';
         $this->assertEquals($y, $x);
 
         $x = '`reStructuredText 中文示例 <?f=20101113-restructuredtext-example.rst>`_';
-        $y = $this->stringUtil->substrIgnoreHtml($x, 71, '');
+        $y = $stringUtil->substrIgnoreHtml($x, 71, '');
         $this->assertEquals($y, $x);
     }
 
 
     public function testToArray()
     {
+        $stringUtil = $this->buildMock();
+
         $x = ' blah ';
         $y = array('blah');
         $y2 = array(' blah ');
-        $this->assertEquals($y, $this->stringUtil->toArray($x));
-        $this->assertEquals($y2, $this->stringUtil->toArray($x, '|', false));
+        $this->assertEquals($y, $stringUtil->toArray($x));
+        $this->assertEquals($y2, $stringUtil->toArray($x, '|', false));
 
         $x = 42;
         $y = array('42');
-        $this->assertEquals($y, $this->stringUtil->toArray($x));
+        $this->assertEquals($y, $stringUtil->toArray($x));
 
         $x = ', a, b, c  , d  , ';
         $this->assertEquals(
             array('a', 'b', 'c', 'd'),
-            $this->stringUtil->toArray($x, ',')
+            $stringUtil->toArray($x, ',')
         );
         $this->assertEquals(
             array(',', 'a,', 'b,', 'c', ',', 'd', ','),
-            $this->stringUtil->toArray($x, ' ')
+            $stringUtil->toArray($x, ' ')
         );
         $this->assertEquals(
             array('', 'a', 'b', 'c',  'd', ''),
-            $this->stringUtil->toArray($x, ',', true, false)
+            $stringUtil->toArray($x, ',', true, false)
         );
         $this->assertEquals(
             array(' a', ' b', ' c  ',  ' d  ', ' '),
-            $this->stringUtil->toArray($x, ',', false, true)
+            $stringUtil->toArray($x, ',', false, true)
         );
     }
 
 
     public function testToCamelCase()
     {
+        $stringUtil = $this->buildMock();
+
         $x = 'camelCase';
         $y = 'camelCase';
-        $this->assertEquals($y, $this->stringUtil->toCamelCase($x));
+        $this->assertEquals($y, $stringUtil->toCamelCase($x));
 
         $x = 'CamelCase';
-        $this->assertEquals($y, $this->stringUtil->toCamelCase($x));
+        $this->assertEquals($y, $stringUtil->toCamelCase($x));
 
         $x = 'camel_case';
-        $this->assertEquals($y, $this->stringUtil->toCamelCase($x));
+        $this->assertEquals($y, $stringUtil->toCamelCase($x));
 
         $x = 'camel .case';
-        $this->assertEquals($y, $this->stringUtil->toCamelCase($x));
+        $this->assertEquals($y, $stringUtil->toCamelCase($x));
     }
 
 
     public function testToSnakeCase()
     {
+        $stringUtil = $this->buildMock();
+
         $x = 'snakeCase';
         $y = 'snake_case';
-        $this->assertEquals($y, $this->stringUtil->toSnakeCase($x));
+        $this->assertEquals($y, $stringUtil->toSnakeCase($x));
 
         $x = 'SnakeCase';
-        $this->assertEquals($y, $this->stringUtil->toSnakeCase($x));
+        $this->assertEquals($y, $stringUtil->toSnakeCase($x));
 
         $x = 'snake_case';
-        $this->assertEquals($y, $this->stringUtil->toSnakeCase($x));
+        $this->assertEquals($y, $stringUtil->toSnakeCase($x));
 
         $x = 'snake .case';
-        $this->assertEquals($y, $this->stringUtil->toSnakeCase($x));
+        $this->assertEquals($y, $stringUtil->toSnakeCase($x));
 
         $y = 'Snake-Case';
-        $this->assertEquals($y, $this->stringUtil->toSnakeCase($x, '-', true));
+        $this->assertEquals($y, $stringUtil->toSnakeCase($x, '-', true));
     }
 
 
     public function testToStudlyCaps()
     {
+        $stringUtil = $this->buildMock();
+
         $x = 'studlyCaps';
         $y = 'StudlyCaps';
-        $this->assertEquals($y, $this->stringUtil->toStudlyCaps($x));
+        $this->assertEquals($y, $stringUtil->toStudlyCaps($x));
 
         $x = 'StudlyCaps';
-        $this->assertEquals($y, $this->stringUtil->toStudlyCaps($x));
+        $this->assertEquals($y, $stringUtil->toStudlyCaps($x));
 
         $x = 'studly_caps';
-        $this->assertEquals($y, $this->stringUtil->toStudlyCaps($x));
+        $this->assertEquals($y, $stringUtil->toStudlyCaps($x));
 
         $x = 'studly .caps';
-        $this->assertEquals($y, $this->stringUtil->toStudlyCaps($x));
+        $this->assertEquals($y, $stringUtil->toStudlyCaps($x));
     }
 }
