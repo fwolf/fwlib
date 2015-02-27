68f34fb1363fe6b843f1991f9fb00c4d5f976b5f
Fwolf <fwolf.aide+git@gmail.com>
2015-02-27 19:04:44 +0800

    Test: New test case to register mocked PHP system functions

---
+++ tests/FwlibTest/AaaMockInitializerTest.php
@@ -0,0 +1,53 @@
+<?php
+namespace FwlibTest;
+
+use Fwlib\Test\Mock\ExtensionLoadedMockTrait;
+use Fwolf\Wrapper\PHPUnit\PHPUnitTestCase;
+
+/**
+ * Initializer of all PHP system function being mocked
+ *
+ * Put in root of test case namespace with name start with 'Aaa' to make sure
+ * PHPUnit run it first.
+ *
+ * The trait need use in every test case want to mock system function. I tried
+ * make a container to store them for reuse, not work.
+ *
+ * The PHPUnit will scan and execute constructor of every test case before run
+ * them(include this one), so code in constructor may use system function first
+ * and make system function mock not work.
+ *
+ * @copyright   Copyright 2015 Fwolf
+ * @license     http://www.gnu.org/licenses/lgpl.html LGPL-3.0+
+ */
+class AaaMockInitializerTest extends PHPUnitTestCase
+{
+    use ExtensionLoadedMockTrait;
+
+
+    /**
+     * Register PHP native function mock here
+     */
+    public function testMockRegister()
+    {
+        $this->buildExtensionLoadedMock('Fwlib\Util');
+
+        $this->assertTrue(true);
+    }
+
+
+    public function testMockSuccessful()
+    {
+        $extensionLoadedMock = $this->buildExtensionLoadedMock('FwlibTest');
+
+        $extensionLoadedMock->enable();
+
+        self::$extensionLoaded = false;
+        $this->assertFalse(extension_loaded('pcre'));
+
+        self::$extensionLoaded = true;
+        $this->assertTrue(extension_loaded('pcre'));
+
+        $extensionLoadedMock->disable();
+    }
+}
