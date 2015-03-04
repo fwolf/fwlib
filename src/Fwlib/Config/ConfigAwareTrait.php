a8fb006f471b3f1fe82b4b2ca97fdd2eb0193169
Fwolf <fwolf.aide+git@gmail.com>
2015-03-04 18:18:27 +0800

    Config: New ConfigAwareTrait

---
+++ src/Fwlib/Config/ConfigAwareTrait.php
@@ -0,0 +1,79 @@
+<?php
+namespace Fwlib\Config;
+
+/**
+ * Trait for class use Config as property
+ *
+ * @copyright   Copyright 2013-2015 Fwolf
+ * @license     http://www.gnu.org/licenses/lgpl.html LGPL-3.0+
+ */
+trait ConfigAwareTrait
+{
+    /**
+     * @var Config
+     */
+    protected $configInstance = null;
+
+
+    /**
+     * Get config value
+     *
+     * @param   string  $key
+     * @param   mixed   $default        Return this if key not exists
+     * @return  mixed
+     */
+    public function getConfig($key, $default = null)
+    {
+        $configInstance = $this->getConfigInstance();
+
+        return $configInstance->get($key, $default);
+    }
+
+
+    /**
+     * @return Config
+     */
+    protected function getConfigInstance()
+    {
+        if (is_null($this->configInstance)) {
+            $config = new Config;
+
+            $config->set($this->getDefaultConfigs());
+
+            $this->configInstance = $config;
+        }
+
+        return $this->configInstance;
+    }
+
+
+    /**
+     * Get default configs
+     *
+     * Will be loaded when get config instance, child class can extend to add
+     * more default configs.
+     *
+     * @return  array
+     */
+    protected function getDefaultConfigs()
+    {
+        return [];
+    }
+
+
+    /**
+     * Set config value
+     *
+     * @param   string|array $key
+     * @param   mixed        $val
+     * @return  static
+     */
+    public function setConfig($key, $val = null)
+    {
+        $configInstance = $this->getConfigInstance();
+
+        $configInstance->set($key, $val);
+
+        return $this;
+    }
+}
---
+++ tests/FwlibTest/Config/ConfigAwareTraitTest.php
@@ -0,0 +1,42 @@
+<?php
+namespace FwlibTest\Config;
+
+use Fwlib\Config\ConfigAwareTrait;
+use Fwolf\Wrapper\PHPUnit\PHPUnitTestCase;
+use PHPUnit_Framework_MockObject_MockObject as MockObject;
+
+/**
+ * @copyright   Copyright 2013-2015 Fwolf
+ * @license     http://www.gnu.org/licenses/lgpl.html LGPL-3.0+
+ */
+class ConfigAwareTraitTest extends PHPUnitTestCase
+{
+    /**
+     * @return MockObject | ConfigAwareTrait
+     */
+    protected function buildMock()
+    {
+        $mock = $this->getMockBuilder(
+            ConfigAwareTrait::class
+        )
+            ->setMethods(null)
+            ->getMockForTrait();
+
+        return $mock;
+    }
+
+
+    public function testNormalSetGet()
+    {
+        $configAware = $this->buildMock();
+
+        $configAware->setConfig([ 'prefix.key1'   => 10,]);
+        $this->assertEquals(10, $configAware->getConfig('prefix.key1'));
+
+        $configAware->setConfig('prefix.key2', 20);
+        $this->assertEquals(
+            ['key1' => 10, 'key2' => 20],
+            $configAware->getConfig('prefix')
+        );
+    }
+}
