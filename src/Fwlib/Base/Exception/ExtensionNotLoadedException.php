f340d7a0cdbac1bcf0a05301633962626a88d95a
Fwolf <fwolf.aide+git@gmail.com>
2015-03-05 15:30:47 +0800

    Base: Minor fix of typo

--- src/Fwlib/Base/Exception/ExtensionNotLoadedException.php
+++ src/Fwlib/Base/Exception/ExtensionNotLoadedException.php
@@ -2,7 +2,7 @@
 namespace Fwlib\Base\Exception;
 
 /**
- * Extension for missing PHP extension
+ * Exception for missing required PHP extension
  *
  * @copyright   Copyright 2015 Fwolf
  * @license     http://www.gnu.org/licenses/lgpl.html LGPL-3.0+
