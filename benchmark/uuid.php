9264c905b282c1787db13b99274c25d2d5008318
Fwolf <fwolf.aide+git@gmail.com>
2015-03-09 01:39:42 +0800

    benchmark: Add Base36Short in uuid benchmark

--- benchmark/uuid.php
+++ benchmark/uuid.php
@@ -3,6 +3,7 @@ require __DIR__ . '/../autoload.php';
 
 use Fwlib\Test\Benchmark;
 use Fwlib\Util\UtilContainer;
+use Fwlib\Util\Uuid\GeneratorInterface;
 
 // Speed test for Uuid generate
 $count = 10000;
@@ -17,15 +18,23 @@ $speed = 0;
 
 $uuidBase16 = $utilContainer->getUuidBase16();
 $uuidBase36 = $utilContainer->getUuidBase36();
+$uuidBase36Short = $utilContainer->getUuidBase36Short();
 $uuidBase62 = $utilContainer->getUuidBase62();
 
+$algorithms = [
+    'Base16',
+    'Base36',
+    'Base36Short',
+    'Base62'
+];
 $arSpeed = [];
-foreach (['Base16', 'Base36', 'Base62'] as $k => $v) {
+foreach ($algorithms as $k => $v) {
     $class = 'Fwlib\\Util\\Uuid\\' . $v;
     $instanceName = 'uuid' . $v;
+    /** @var GeneratorInterface $instance */
     $instance = $$instanceName;
 
-    $v = str_pad($v, 10, ' ', STR_PAD_RIGHT);   // For display later
+    $v = str_pad($v, 11, ' ', STR_PAD_RIGHT);   // For display later
 
     for ($i = 0; $i < $count; $i ++) {
         $instance->generate();
@@ -49,14 +58,17 @@ echo $rs;
 
 
 $env = $utilContainer->getEnv();
-$env->ecl('Base16 without check digit: ' . $uuidBase16->generate('10', null, false));
-$env->ecl('Base16 with    check digit: ' . $uuidBase16->generate('10', null, true));
+$env->ecl('Base16      without check digit: ' . $uuidBase16->generate('10', null, false));
+$env->ecl('Base16      with    check digit: ' . $uuidBase16->generate('10', null, true));
 
-$env->ecl('Base16 without check digit: ' . $uuidBase16->generateWithSeparator('10', null, false));
-$env->ecl('Base16 with    check digit: ' . $uuidBase16->generateWithSeparator('10', null, true));
+$env->ecl('Base16      without check digit: ' . $uuidBase16->generateWithSeparator('10', null, false));
+$env->ecl('Base16      with    check digit: ' . $uuidBase16->generateWithSeparator('10', null, true));
 
-$env->ecl('Base36 without check digit: ' . $uuidBase36->generate('10', null, false));
-$env->ecl('Base36 with    check digit: ' . $uuidBase36->generate('10', null, true));
+$env->ecl('Base36      without check digit: ' . $uuidBase36->generate('10', null, false));
+$env->ecl('Base36      with    check digit: ' . $uuidBase36->generate('10', null, true));
 
-$env->ecl('Base62 without check digit: ' . $uuidBase62->generate('10', null, false));
-$env->ecl('Base62 with    check digit: ' . $uuidBase62->generate('10', null, true));
+$env->ecl('Base36Short without check digit: ' . $uuidBase36Short->generate('1', null, false));
+$env->ecl('Base36Short with    check digit: ' . $uuidBase36Short->generate('1', null, true));
+
+$env->ecl('Base62      without check digit: ' . $uuidBase62->generate('10', null, false));
+$env->ecl('Base62      with    check digit: ' . $uuidBase62->generate('10', null, true));
