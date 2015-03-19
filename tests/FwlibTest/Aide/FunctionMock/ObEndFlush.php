e9d309c9416599ae5314b3066c83c4e8127c3d8f
Fwolf <fwolf.aide+git@gmail.com>
2015-03-19 16:31:49 +0800

    Test: New mock of ob_end_flush()

---
+++ tests/FwlibTest/Aide/FunctionMock/ObEndFlush.php
@@ -0,0 +1,31 @@
+<?php
+namespace FwlibTest\Aide\FunctionMock;
+
+use FwlibTest\Aide\FunctionMockWrapperInterface;
+use FwlibTest\Aide\FunctionMockWrapperTrait;
+
+/**
+ * @copyright   Copyright 2015 Fwolf
+ * @license     http://www.gnu.org/licenses/lgpl.html LGPL-3.0+
+ */
+class ObEndFlush implements FunctionMockWrapperInterface
+{
+    use FunctionMockWrapperTrait;
+
+
+    /** @type string */
+    public $function = 'ob_end_flush';
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function build($namespace, $enabled = false)
+    {
+        $callback = function() use ($namespace) {
+            self::$results[$namespace] = true;
+        };
+
+        return $this->buildFunctionMock($namespace, $callback, $enabled);
+    }
+}
