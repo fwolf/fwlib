07d8b0a5d7bfee821b6af1b6ea9f49352ad27e60
Fwolf <fwolf.aide+git@gmail.com>
2015-03-20 00:27:48 +0800

    Test: New mock of class_exists()

---
+++ tests/FwlibTest/Aide/FunctionMock/ClassExists.php
@@ -0,0 +1,17 @@
+<?php
+namespace FwlibTest\Aide\FunctionMock;
+
+use FwlibTest\Aide\FunctionMockWrapperInterface;
+
+/**
+ * @copyright   Copyright 2015 Fwolf
+ * @license     http://www.gnu.org/licenses/lgpl.html LGPL-3.0+
+ */
+class ClassExists implements FunctionMockWrapperInterface
+{
+    use DirectReturnTrait;
+
+
+    /** @type string */
+    public $function = 'class_exists';
+}
