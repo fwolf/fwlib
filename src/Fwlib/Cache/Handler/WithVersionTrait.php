44f405999de7d72bd819f68dd66a77851bff7f66
Fwolf <fwolf.aide+git@gmail.com>
2015-03-24 17:35:30 +0800

    Cache: Extract WithVersionTrait from Cache

---
+++ src/Fwlib/Cache/Handler/WithVersionTrait.php
@@ -0,0 +1,67 @@
+<?php
+namespace Fwlib\Cache\Handler;
+
+/**
+ * Trait for cache store data with version, for batch delete
+ *
+ * Eg: A multiple page list is stored in cache, its key is 'foo', at beginning
+ * its version is 1, this version number is also stored in cache, with key
+ * 'foo-ver'. When retrieve every page of this list, we should use key
+ * 'foo-ver-1-p-1' for page 1. Now we want to change all page of this list, so
+ * we increase version of this list to 2, and then the first page should get by
+ * key 'foo-ver-2-p-1'. Need not delete all page data of version 1, and some
+ * cache system does not support this(memcached).
+ *
+ * @property    string  $versionSuffix  Append this to key, got key for version
+ * @method      string  get($key)
+ * @method      static  set($key, $value)
+ *
+ * @copyright   Copyright 2012-2015 Fwolf
+ * @license     http://www.gnu.org/licenses/lgpl.html LGPL-3.0+
+ */
+trait WithVersionTrait
+{
+    /**
+     * Get current version number
+     *
+     * @param   string  $key
+     * @return  int
+     */
+    public function getVersion($key)
+    {
+        $versionKey = $key . $this->versionSuffix;
+        $version = $this->get($versionKey);
+
+        if (empty($version)) {
+            $version = 1;
+            $this->set($versionKey, $version, 0);
+        }
+
+        return intval($version);
+    }
+
+
+    /**
+     * Increase version and return new version number
+     *
+     * @param   string  $key
+     * @param   int     $increment
+     * @param   int     $max
+     * @return  int
+     */
+    public function increaseVersion($key, $increment = 1, $max = 65535)
+    {
+        $version = $this->getVersion($key);
+
+        $version += $increment;
+
+        if ($max < $version) {
+            $version = 1;
+        }
+
+        $versionKey = $key . $this->versionSuffix;
+        $this->set($versionKey, $version, 0);
+
+        return $version;
+    }
+}
---
+++ tests/FwlibTest/Cache/Handler/WithVersionTraitTest.php
@@ -0,0 +1,63 @@
+<?php
+namespace FwlibTest\Cache\Handler;
+
+use Fwlib\Cache\Handler\WithVersionTrait;
+use Fwolf\Wrapper\PHPUnit\PHPUnitTestCase;
+use PHPUnit_Framework_MockObject_MockObject as MockObject;
+
+/**
+ * @copyright   Copyright 2015 Fwolf
+ * @license     http://www.gnu.org/licenses/lgpl.html LGPL-3.0+
+ */
+class WithVersionTraitTest extends PHPUnitTestCase
+{
+    /**
+     * @var int[]
+     */
+    protected $versions = [];
+
+
+    /**
+     * @return MockObject | WithVersionTrait
+     */
+    protected function buildMock()
+    {
+        $mock = $this->getMockBuilder(WithVersionTrait::class)
+            ->setMethods(['get', 'set'])
+            ->getMockForTrait();
+
+        $mock->expects($this->any())
+            ->method('get')
+            ->willReturnCallback(function($key) {
+                return array_key_exists($key, $this->versions)
+                    ? $this->versions[$key]
+                    : null;
+            });
+
+        $mock->expects($this->any())
+            ->method('set')
+            ->willReturnCallback(function($key, $value) {
+                $this->versions[$key] = $value;
+            });
+
+        /** @noinspection PhpUndefinedFieldInspection */
+        $mock->versionSuffix = '-ver';
+
+        return $mock;
+    }
+
+
+    public function test()
+    {
+        $handler = $this->buildMock();
+
+        $key = 'foo';
+        $this->assertEquals(1, $handler->getVersion($key));
+        $this->assertEquals(2, $handler->increaseVersion($key));
+        $this->assertEquals(2, $handler->getVersion($key));
+
+        $this->versions['foo-ver'] = 65534;
+        $this->assertEquals(65535, $handler->increaseVersion($key));
+        $this->assertEquals(1, $handler->increaseVersion($key));
+    }
+}
