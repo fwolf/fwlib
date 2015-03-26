c094cbe9e5c07625deeca1ffe935dc6a305709a8
Fwolf <fwolf.aide+git@gmail.com>
2015-03-26 18:33:51 +0800

    Test: Add method getFunctionMock()

--- tests/FwlibTest/Aide/FunctionMockAwareTrait.php
+++ tests/FwlibTest/Aide/FunctionMockAwareTrait.php
@@ -2,7 +2,9 @@
 namespace FwlibTest\Aide;
 
 /**
- * Trait for function mock factory client
+ * Trait for function mock client
+ *
+ * Function mock can get from factory, or use quick getter.
  *
  * @see \Fwlib\Base\ServiceContainerAwareTrait
  *
@@ -15,6 +17,40 @@ trait FunctionMockAwareTrait
      * @var FunctionMockFactory
      */
     protected $functionMockFactory = null;
+
+
+    /**
+     * Quick getter of function mock
+     *
+     * Mock is default enabled.
+     *
+     * Default namespace is corresponding to test class namespace, with 'Test'
+     * suffix removed from first section of namespace.
+     *
+     * @param   string  $function
+     * @param   bool    $enabled
+     * @param   string  $namespace
+     * @return  FunctionMockWrapperInterface
+     */
+    protected function getFunctionMock(
+        $function,
+        $enabled = true,
+        $namespace = null
+    ) {
+        if (is_null($namespace)) {
+            $namespace = get_class($this);
+            $namespace = implode(
+                '\\',
+                array_slice(explode('\\', $namespace), 0, -1)
+            );
+
+            $namespace = preg_replace('/^(\S+)Test\\\/', '$1\\', $namespace);
+        }
+
+        $factory = $this->getFunctionMockFactory($namespace);
+
+        return $factory->get(null, $function, $enabled);
+    }
 
 
     /**
