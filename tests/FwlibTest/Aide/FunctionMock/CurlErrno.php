e00ca63db4a3e9a46a6cf0b9f76990f556c5687b
Fwolf <fwolf.aide+git@gmail.com>
2015-03-27 16:37:20 +0800

    Test: New function mock of curl_errno(), curl_error()

---
+++ tests/FwlibTest/Aide/FunctionMock/CurlErrno.php
@@ -0,0 +1,18 @@
+<?php
+namespace FwlibTest\Aide\FunctionMock;
+
+use FwlibTest\Aide\FunctionMock\Helper\DirectReturnTrait;
+use FwlibTest\Aide\FunctionMockWrapperInterface;
+
+/**
+ * @copyright   Copyright 2015 Fwolf
+ * @license     http://www.gnu.org/licenses/lgpl.html LGPL-3.0+
+ */
+class CurlErrno implements FunctionMockWrapperInterface
+{
+    use DirectReturnTrait;
+
+
+    /** @type string */
+    public $function = 'curl_errno';
+}
