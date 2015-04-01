ce870efd56bb8194710b3d24e673e47d52c789bc
Fwolf <fwolf.aide+git@gmail.com>
2015-04-01 23:50:41 +0800

    Revert "Html: Remove parameter accessors from RequestInterface"
    Let it be common ways.
    
    This reverts commit 33d44297bd5c973db8ba7956f942d6af74eba22d.

--- src/Fwlib/Html/ListView/RequestInterface.php
+++ src/Fwlib/Html/ListView/RequestInterface.php
@@ -26,11 +26,32 @@ interface RequestInterface
     public function getOrderBy();
 
     /**
+     * Get parameter of order by direction
+     *
+     * @return  string
+     */
+    public function getOrderByDirectionParameter();
+
+    /**
+     * Get parameter of order by column
+     *
+     * @return  string
+     */
+    public function getOrderByParameter();
+
+    /**
      * Get current page number
      *
      * @return  int
      */
     public function getPage();
+
+    /**
+     * Get parameter of current page
+     *
+     * @return  string
+     */
+    public function getPageParameter();
 
     /**
      * Get current page size from request
@@ -40,6 +61,13 @@ interface RequestInterface
      * @return  int
      */
     public function getPageSize();
+
+    /**
+     * Get parameter of list page size
+     *
+     * @return  mixed
+     */
+    public function getPageSizeParameter();
 
     /**
      * Getter of request source
@@ -57,6 +85,38 @@ interface RequestInterface
      * @return  static
      */
     public function setBaseUrl($baseUrl);
+
+    /**
+     * Set parameter of order by direction
+     *
+     * @param   string $orderByDirParameter
+     * @return  static
+     */
+    public function setOrderByDirectionParameter($orderByDirParameter);
+
+    /**
+     * Set parameter of order by column
+     *
+     * @param   string $orderByParameter
+     * @return  static
+     */
+    public function setOrderByParameter($orderByParameter);
+
+    /**
+     * Set parameter of current page
+     *
+     * @param   string $pageParameter
+     * @return  static
+     */
+    public function setPageParameter($pageParameter);
+
+    /**
+     * Set parameter of list page size
+     *
+     * @param   mixed $pageSizeParameter
+     * @return  static
+     */
+    public function setPageSizeParameter($pageSizeParameter);
 
     /**
      * Setter of request source
