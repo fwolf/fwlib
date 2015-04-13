0e153cb56213b675d0a13d333c08123416406a3d
Fwolf <fwolf.aide+git@gmail.com>
2015-04-13 23:21:09 +0800

    Html: New skeleton AbstractRetriever and interface

---
+++ src/Fwlib/Html/ListView/AbstractRetriever.php
@@ -0,0 +1,25 @@
+<?php
+namespace Fwlib\Html\ListView;
+
+use Fwlib\Config\ConfigAwareTrait;
+
+/**
+ * @copyright   Copyright 2015 Fwolf
+ * @license     http://www.gnu.org/licenses/lgpl.html LGPL-3.0+
+ */
+abstract class AbstractRetriever implements RetrieverInterface
+{
+    use ConfigAwareTrait;
+
+
+    /**
+     * {@inheritdoc}
+     */
+    abstract public function getListBody();
+
+
+    /**
+     * {@inheritdoc}
+     */
+    abstract public function getRowCount();
+}
---
+++ src/Fwlib/Html/ListView/RetrieverInterface.php
@@ -0,0 +1,45 @@
+<?php
+namespace Fwlib\Html\ListView;
+
+/**
+ * RetrieverInterface
+ *
+ * Retrieve list body and row count from backend, use shared config with
+ * {@see ListView} and other components.
+ *
+ * @copyright   Copyright 2015 Fwolf
+ * @license     http://www.gnu.org/licenses/lgpl.html LGPL-3.0+
+ */
+interface RetrieverInterface
+{
+    /**
+     * Get list body
+     *
+     * @return  array[]
+     */
+    public function getListBody();
+
+    /**
+     * Get row count
+     *
+     * @return  int
+     */
+    public function getRowCount();
+
+    /**
+     * Set single config value
+     *
+     * @param   string       $key
+     * @param   mixed        $val
+     * @return  static
+     */
+    public function setConfig($key, $val);
+
+    /**
+     * Batch set config values
+     *
+     * @param   array   $configs
+     * @return  static
+     */
+    public function setConfigs(array $configs);
+}
