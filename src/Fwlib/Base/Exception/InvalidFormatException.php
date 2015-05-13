4d525e031a48756e4dba0f9188cb89c1a925bf0e
Fwolf <fwolf.aide+git@gmail.com>
2015-05-14 01:13:52 +0800

    Base: New InvalidFormatException

---
+++ src/Fwlib/Base/Exception/InvalidFormatException.php
@@ -0,0 +1,12 @@
+<?php
+namespace Fwlib\Base\Exception;
+
+/**
+ * Exception for format error
+ *
+ * @copyright   Copyright 2015 Fwolf
+ * @license     http://www.gnu.org/licenses/lgpl.html LGPL-3.0+
+ */
+class InvalidFormatException extends \Exception
+{
+}
