a2fdf675b3f68aa1b540d0e0c418618622d72ade
Fwolf <fwolf.aide+git@gmail.com>
2015-05-22 16:35:41 +0800

    Validator: New InvalidRuleStringException

---
+++ src/Fwlib/Validator/Exception/InvalidRuleStringException.php
@@ -0,0 +1,10 @@
+<?php
+namespace Fwlib\Validator\Exception;
+
+/**
+ * @copyright   Copyright 2015 Fwolf
+ * @license     http://www.gnu.org/licenses/lgpl.html LGPL-3.0+
+ */
+class InvalidRuleStringException extends \Exception
+{
+}
