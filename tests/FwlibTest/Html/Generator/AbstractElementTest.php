2c7f09726cd7b73f2f6df6507a6f622a8b18e292
Fwolf <fwolf.aide+git@gmail.com>
2015-07-03 00:43:22 +0800

    Html: Minor change of add copyright owner

--- tests/FwlibTest/Html/Generator/AbstractElementTest.php
+++ tests/FwlibTest/Html/Generator/AbstractElementTest.php
@@ -8,7 +8,7 @@ use Fwolf\Wrapper\PHPUnit\PHPUnitTestCase;
 use PHPUnit_Framework_MockObject_MockObject as MockObject;
 
 /**
- * @copyright   Copyright 2014-2015
+ * @copyright   Copyright 2014-2015 Fwolf
  * @license     http://www.gnu.org/licenses/lgpl.html LGPL-3.0+
  */
 class AbstractElementTest extends PHPunitTestCase
