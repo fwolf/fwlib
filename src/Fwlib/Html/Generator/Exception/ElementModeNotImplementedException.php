7178d61a8085a172c96e7aa43ec6b13a604d29af
Fwolf <fwolf.aide+git@gmail.com>
2015-07-08 00:59:46 +0800

    Html: New ElementModeNotImplementedException
    This class is miss in previous commit.

---
+++ src/Fwlib/Html/Generator/Exception/ElementModeNotImplementedException.php
@@ -0,0 +1,12 @@
+<?php
+namespace Fwlib\Html\Generator\Exception;
+
+/**
+ * Exception for element not implement required mode
+ *
+ * @copyright   Copyright 2015 Fwolf
+ * @license     http://www.gnu.org/licenses/lgpl.html LGPL-3.0+
+ */
+class ElementModeNotImplementedException extends \Exception
+{
+}
