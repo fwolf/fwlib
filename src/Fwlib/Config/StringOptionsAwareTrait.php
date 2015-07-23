eccb41bec7cb069c0bcd4b360672e608ef538fd1
Fwolf <fwolf.aide+git@gmail.com>
2015-07-24 01:25:01 +0800

    Config: Minor change of phpdoc

--- src/Fwlib/Config/StringOptionsAwareTrait.php
+++ src/Fwlib/Config/StringOptionsAwareTrait.php
@@ -4,9 +4,9 @@ namespace Fwlib\Config;
 /**
  * Trait for Config aware class also accept StringOptions
  *
- * @see ConfigAwareTrait
+ * @see         ConfigAwareTrait
  *
- * @method  static  setConfigs(array $configs)
+ * @method  static $this   setConfigs(array $configs)
  *
  * @copyright   Copyright 2015 Fwolf
  * @license     http://www.gnu.org/licenses/lgpl.html LGPL-3.0+
@@ -16,7 +16,7 @@ trait StringOptionsAwareTrait
     /**
      * Set configs with string style
      *
-     * @param   string  $optionString
+     * @param   string $optionString
      * @return  static
      */
     public function setStringOptions($optionString)
