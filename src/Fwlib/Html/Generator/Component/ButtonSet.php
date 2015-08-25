28837020286f8748aaea6f7355996c35ac12666f
Fwolf <fwolf.aide+git@gmail.com>
2015-08-25 23:51:07 +0800

    Html: New ButtonSet component

---
+++ src/Fwlib/Html/Generator/Component/ButtonSet.php
@@ -0,0 +1,248 @@
+<?php
+namespace Fwlib\Html\Generator\Component;
+
+use Fwlib\Html\Generator\Element\Button;
+use Fwlib\Util\UtilContainer;
+
+/**
+ * Html button set
+ *
+ * @copyright   Copyright 2014-2015 Fwolf
+ * @license     http://www.gnu.org/licenses/lgpl.html LGPL-3.0+
+ */
+class ButtonSet
+{
+    /**
+     * Button array, should index by button name
+     *
+     * @var Button[]
+     */
+    protected $buttons = [];
+
+    /**
+     * Container class in html
+     *
+     * @var string
+     */
+    protected $containerClass = 'form__buttons';
+
+    /**
+     * Container id in html
+     *
+     * @var string
+     */
+    protected $containerId = '';
+
+    /**
+     * Container tag in html
+     *
+     * @var string
+     */
+    protected $containerTag = 'div';
+
+    /**
+     * @var int
+     */
+    protected $indent = 0;
+
+    /**
+     * @var int
+     */
+    protected $sleepTime = 0;
+
+
+    /**
+     * Add/append a button
+     *
+     * @param   Button $button
+     * @return  static
+     */
+    public function add(Button $button)
+    {
+        $name = $button->getName();
+        $newButtonAr = (empty($name)) ? [$button] : [$name => $button];
+
+        $this->buttons = array_merge($this->buttons, $newButtonAr);
+
+        return $this;
+    }
+
+
+    /**
+     * Apply container to html of buttons
+     *
+     * @param   string $html Buttons html
+     * @return  string
+     */
+    protected function applyContainer($html)
+    {
+        $classHtml = empty($this->containerClass) ? ''
+            : " class='{$this->containerClass}'";
+        $idHtml = empty($this->containerId) ? ''
+            : " id='{$this->containerId}'";
+
+        $containerClass = $this->containerClass;
+        $sleepTime = $this->sleepTime;
+        if (!empty($containerClass) && 0 < $sleepTime) {
+            $sleepJs = $this->getSleepJs($containerClass, $sleepTime);
+        } else {
+            $sleepJs = '';
+        }
+
+        $stringUtil = UtilContainer::getInstance()->getString();
+        $html = $stringUtil->indent($html, 2);
+
+        $html = <<<TAG
+<{$this->containerTag}{$classHtml}{$idHtml}>
+$html
+</{$this->containerTag}>
+
+{$sleepJs}
+TAG;
+
+        $html = $stringUtil->indent($html, $this->indent);
+
+        return $html;
+    }
+
+
+    /**
+     * Get output of button set
+     *
+     * @return  string
+     */
+    public function getOutput()
+    {
+        $html = '';
+
+        foreach ($this->buttons as $button) {
+            $html .= $button->getOutput() . "\n";
+        }
+
+        $html = $this->applyContainer(trim($html));
+
+        return $html;
+    }
+
+
+    /**
+     * Disable all submit button in set
+     *
+     * Disabled submit button will prevent form submit, so put it in submit
+     * event handler.
+     *
+     * @link https://stackoverflow.com/questions/5691054
+     *
+     * @param   string $containerClass
+     * @param   int    $sleepTime
+     * @return  string
+     */
+    protected function getSleepJs($containerClass, $sleepTime)
+    {
+        return <<<TAG
+<script type='text/javascript'>
+(function () {
+  $('.{$containerClass} input[type=submit]').on('click', function () {
+    $(this).closest('form').submit(function () {
+      $('.{$containerClass} input[type=submit]').attr('disabled', true);
+      setTimeout(function () {
+        $('.{$containerClass} input[type=submit]').removeAttr('disabled');
+      }, {$sleepTime});
+    });
+  });
+}) ();
+</script>
+TAG;
+    }
+
+
+    /**
+     * Prepend a button
+     *
+     * @param   Button $button
+     * @return  static
+     */
+    public function prepend(Button $button)
+    {
+        $name = $button->getName();
+        $newButtonAr = (empty($name)) ? [$button] : [$name => $button];
+
+        $this->buttons = array_merge($newButtonAr, $this->buttons);
+
+        return $this;
+    }
+
+
+    /**
+     * Remove a button
+     *
+     * @param   string $name
+     * @return  static
+     */
+    public function remove($name)
+    {
+        unset($this->buttons[$name]);
+
+        return $this;
+    }
+
+
+    /**
+     * @param   string $containerClass
+     * @return  static
+     */
+    public function setContainerClass($containerClass)
+    {
+        $this->containerClass = $containerClass;
+
+        return $this;
+    }
+
+
+    /**
+     * @param   string $idStr
+     * @return  static
+     */
+    public function setContainerId($idStr)
+    {
+        $this->containerId = $idStr;
+
+        return $this;
+    }
+
+
+    /**
+     * @param   string $containerTag
+     * @return  static
+     */
+    public function setContainerTag($containerTag)
+    {
+        $this->containerTag = $containerTag;
+
+        return $this;
+    }
+
+
+    /**
+     * @param   int $indent
+     * @return  static
+     */
+    public function setIndent($indent)
+    {
+        $this->indent = $indent;
+
+        return $this;
+    }
+
+
+    /**
+     * @param   int $sleepTime
+     * @return  static
+     */
+    public function setSleepTime($sleepTime)
+    {
+        $this->sleepTime = $sleepTime;
+
+        return $this;
+    }
+}
---
+++ tests/FwlibTest/Html/Generator/Component/ButtonSetTest.php
@@ -0,0 +1,127 @@
+<?php
+namespace FwlibTest\Html\Generator\Component;
+
+use Fwlib\Html\Generator\Component\ButtonSet;
+use Fwlib\Html\Generator\Element\Button;
+use Fwolf\Wrapper\PHPUnit\PHPUnitTestCase;
+use PHPUnit_Framework_MockObject_MockObject as MockObject;
+
+/**
+ * @copyright   Copyright 2015 Fwolf
+ * @license     http://www.gnu.org/licenses/lgpl.html LGPL-3.0+
+ */
+class ButtonSetTest extends PHPUnitTestCase
+{
+    /**
+     * @param   string[] $methods
+     * @return  MockObject|Button
+     */
+    protected function buildButtonMock(array $methods = null)
+    {
+        $mock = $this->getMock(
+            Button::class,
+            $methods
+        );
+
+        return $mock;
+    }
+
+
+    /**
+     * @param   string[] $methods
+     * @return  MockObject|ButtonSet
+     */
+    protected function buildMock(array $methods = null)
+    {
+        $mock = $this->getMock(
+            ButtonSet::class,
+            $methods
+        );
+
+        return $mock;
+    }
+
+
+    public function testAddAndPrependAndRemove()
+    {
+        $buttonSet = $this->buildMock();
+
+
+        $button = $this->buildButtonMock()
+            ->setName('foo1');
+        $buttonSet->add($button);
+        $buttons = $this->reflectionGet($buttonSet, 'buttons');
+        $this->assertEquals(1, count($buttons));
+
+
+        $button = $this->buildButtonMock()
+            ->setName('foo2');
+        $buttonSet->add($button);
+        $buttons = $this->reflectionGet($buttonSet, 'buttons');
+        $this->assertEquals(2, count($buttons));
+
+
+        $button = $this->buildButtonMock()
+            ->setName('foo3');
+        $buttonSet->prepend($button);
+        $buttons = $this->reflectionGet($buttonSet, 'buttons');
+        $this->assertEquals(3, count($buttons));
+
+        $this->assertEqualArray(
+            ['foo3', 'foo1', 'foo2'],
+            array_keys($buttons)
+        );
+
+        $buttonSet->remove('foo2')
+            ->remove('foo3');
+        $buttons = $this->reflectionGet($buttonSet, 'buttons');
+        $this->assertEqualArray(['foo1'], array_keys($buttons));
+    }
+
+
+    public function testGetOutput()
+    {
+        $button = $this->buildButtonMock(['getOutput']);
+        $button->expects($this->once())
+            ->method('getOutput');
+
+        $buttonSet = $this->buildMock();
+        $buttonSet->add($button);
+
+        $buttonSet->setContainerClass('foo')
+            ->setContainerId('bar')
+            ->setContainerTag('tag')
+            ->setIndent(3);
+
+        $output = $buttonSet->getOutput();
+
+        $this->assertRegExp("/'foo'/", $output);
+        $this->assertRegExp("/'bar'/", $output);
+        $this->assertRegExp("/   <tag /", $output);
+        $this->assertRegExp("/   <\\/tag>/", $output);
+    }
+
+
+    public function testGetOutputWithJs()
+    {
+        $buttonSet = $this->buildMock();
+
+        $buttonSet->setContainerClass('foo')
+            ->setSleepTime(3000);
+        $this->assertRegExp('/<script /', $buttonSet->getOutput());
+    }
+
+
+    public function testGetOutputWithoutJs()
+    {
+        $buttonSet = $this->buildMock();
+
+        $buttonSet->setContainerClass('')
+            ->setSleepTime(3000);
+        $this->assertNotRegExp('/<script /', $buttonSet->getOutput());
+
+        $buttonSet->setContainerClass('foo')
+            ->setSleepTime(0);
+        $this->assertNotRegExp('/<script /', $buttonSet->getOutput());
+    }
+}
