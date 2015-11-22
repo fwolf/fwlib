e1e52ea8d4accc3428c0729e034a9896117c5377
Fwolf <fwolf.aide+git@gmail.com>
2015-11-22 18:33:50 +0800

    Html: New DropDownSelect element

---
+++ src/Fwlib/Html/Generator/Element/DropDownSelect.php
@@ -0,0 +1,162 @@
+<?php
+namespace Fwlib\Html\Generator\Element;
+
+use Fwlib\Html\Generator\AbstractElement;
+use Fwlib\Html\Generator\ElementMode;
+use Fwlib\Html\Generator\Helper\GetTitleClassAndIdTrait;
+
+/**
+ * Drop down select box
+ *
+ * @copyright   Copyright 2015 Fwolf
+ * @license     http://www.gnu.org/licenses/lgpl.html LGPL-3.0+
+ */
+class DropDownSelect extends AbstractElement
+{
+    use GetTitleClassAndIdTrait;
+
+
+    const KEY_TAG = 'tag';
+
+    const KEY_ITEMS = 'items';
+
+    const KEY_PROMPT = 'prompt';
+
+    const KEY_INVALID = 'invalid';
+
+
+    /**
+     * {@inheritdoc}
+     *
+     * Configs
+     * - tag: Use div or p or none html tag in show mode.
+     * - items: Select able items, {value: title}.
+     * - prompt: Show welcome message like 'please select'.
+     * - invalid: Show when value not found in items.
+     */
+    protected function getDefaultConfigs()
+    {
+        $configs = parent::getDefaultConfigs();
+
+        return array_merge($configs, [
+            self::KEY_TAG     => 'span',
+            self::KEY_ITEMS   => [],
+            self::KEY_PROMPT  => 'Please select',
+            self::KEY_INVALID => 'Invalid Item',
+        ]);
+    }
+
+
+    /**
+     * @return  string
+     */
+    protected function getInvalid()
+    {
+        return $this->getConfig(self::KEY_INVALID);
+    }
+
+
+    /**
+     * @return  string[]
+     */
+    protected function getItems()
+    {
+        return $this->getConfig(self::KEY_ITEMS);
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getOutputForEditMode()
+    {
+        $items = $this->getItems();
+        $prompt = $this->getPrompt();
+
+        $output = "<select" .
+            $this->getClassHtml() .
+            $this->getIdHtml() .
+            $this->getRawAttributesHtml() .
+            ">";
+
+        $titleClass = $this->getTitleClass();
+        $titleClassHtml = $this->getClassHtml($titleClass);
+
+        if (!empty($prompt)) {
+            $output .= <<<TAG
+
+  <option value=''{$titleClassHtml}>$prompt</option>
+TAG;
+        }
+
+        foreach ($items as $value => $title) {
+            $selected = ($value == $this->getValue())
+                ? " selected='selected'" : '';
+            $titleId = $this->getTitleId($value);
+            $titleIdHtml = $this->getIdHtml($titleId);
+            $output .= <<<TAG
+
+  <option value='$value'{$selected}{$titleClassHtml}{$titleIdHtml}>{$title}</option>
+TAG;
+        }
+
+        $output .= "
+</select>";
+
+        return $output;
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getOutputForShowMode()
+    {
+        $items = $this->getItems();
+        $value = $this->getValue();
+        $title = array_key_exists($value, $items)
+            ? $items[$value]
+            : $this->getInvalid();
+
+        $tag = $this->getTag();
+
+        if (empty($tag)) {
+            return $title;
+        }
+
+        // Only one value will be shown, so title id not include value
+        $output = "<input type='hidden'" .
+            $this->getIdHtml() .
+            $this->getNameHtml() .
+            $this->getValueHtml(ElementMode::EDIT) .
+            $this->getRawAttributes() .
+            " />\n" .
+            "<$tag" .
+            $this->getClassHtml($this->getTitleClass()) .
+            $this->getIdHtml($this->getTitleId()) .
+            $this->getRawAttributesHtml() .
+            ">" .
+            $title .
+            "</$tag>";
+
+        return $output;
+    }
+
+
+    /**
+     * @return  string
+     */
+    protected function getPrompt()
+    {
+        return $this->getConfig(self::KEY_PROMPT);
+    }
+
+
+    /**
+     * @return  string
+     */
+    protected function getTag()
+    {
+        return $this->getConfig(self::KEY_TAG);
+    }
+}
---
+++ tests/FwlibTest/Html/Generator/Element/DropDownSelectTest.php
@@ -0,0 +1,84 @@
+<?php
+namespace FwlibTest\Html\Generator\Element;
+
+use Fwlib\Html\Generator\Element\DropDownSelect;
+use Fwlib\Html\Generator\ElementMode;
+use Fwolf\Wrapper\PHPUnit\PHPUnitTestCase;
+use PHPUnit_Framework_MockObject_MockObject as MockObject;
+
+/**
+ * @copyright   Copyright 2015 Fwolf
+ * @license     http://www.gnu.org/licenses/lgpl.html LGPL-3.0+
+ */
+class DropDownSelectTest extends PHPUnitTestCase
+{
+    /**
+     * @param   string[] $methods
+     * @return  MockObject|DropDownSelect
+     */
+    protected function buildMock(array $methods = null)
+    {
+        /** @var MockObject|DropDownSelect $mock */
+        $mock = $this->getMock(
+            DropDownSelect::class,
+            $methods
+        );
+
+        $mock->setClass('foo')
+            ->setId('bar')
+            ->setConfig(DropDownSelect::KEY_ITEMS, [
+                1 => 'A',
+                2 => 'B',
+            ]);
+
+        return $mock;
+    }
+
+
+    public function testGetOutputForEditMode()
+    {
+        $element = $this->buildMock();
+
+        $element->setConfig(DropDownSelect::KEY_PROMPT, 'Select One')
+            ->setValue(2);
+
+        $expected = <<<TAG
+<select class='foo' id='bar'>
+  <option value='' class='foo__title'>Select One</option>
+  <option value='1' class='foo__title' id='bar__title--1'>A</option>
+  <option value='2' selected='selected' class='foo__title' id='bar__title--2'>B</option>
+</select>
+TAG;
+
+        $this->assertEquals($expected, $element->getOutput(ElementMode::EDIT));
+    }
+
+
+    public function testGetOutputForShowMode()
+    {
+        $element = $this->buildMock();
+
+        $element->setConfig(DropDownSelect::KEY_TAG, 'p');
+
+        $expected = <<<TAG
+<input type='hidden' id='bar' value='' />
+<p class='foo__title' id='bar__title'>Invalid Item</p>
+TAG;
+        $this->assertEquals($expected, $element->getOutput(ElementMode::SHOW));
+
+
+        $element->setValue(1);
+        $expected = <<<TAG
+<input type='hidden' id='bar' value='1' />
+<p class='foo__title' id='bar__title'>A</p>
+TAG;
+        $this->assertEquals($expected, $element->getOutput(ElementMode::SHOW));
+
+
+        $element->setConfig(DropDownSelect::KEY_TAG, '');
+        $expected = <<<TAG
+A
+TAG;
+        $this->assertEquals($expected, $element->getOutput(ElementMode::SHOW));
+    }
+}
