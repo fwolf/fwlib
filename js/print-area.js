a0c1a65155b6d1579efb9b6cef02e1b3487d49ee
Fwolf <fwolf.aide+git@gmail.com>
2016-06-28 17:45:55 +0800

    js: Minor change of indent by space

--- js/print-area.js
+++ js/print-area.js
@@ -19,56 +19,56 @@
  * @param	{object} o_cfg
  */
 function PrintArea (o_cfg) {
-	/* Parse param */
-	if ('undefined' == typeof(o_cfg.css_text))
-		o_cfg.css_text = '';
-	if ('undefined' == typeof(o_cfg.css_url))
-		o_cfg.css_url = [];
-	if ('undefined' == typeof(o_cfg.id_frame))
-		o_cfg.id_frame = 'frame_print';
+  /* Parse param */
+  if ('undefined' == typeof(o_cfg.css_text))
+    o_cfg.css_text = '';
+  if ('undefined' == typeof(o_cfg.css_url))
+    o_cfg.css_url = [];
+  if ('undefined' == typeof(o_cfg.id_frame))
+    o_cfg.id_frame = 'frame_print';
 
-	/* Create print frame if not exists */
-	if ('undefined' === typeof($('#' + o_cfg.id_frame).attr('id'))) {
-		$('body').append('<iframe id=\'' + o_cfg.id_frame + '\' \
-			name=\'' + o_cfg.id_frame + '\' \
-			width=\'0\' height=\'0\' frameborder=\'0\' \
-			src=\'about:blank\' ></iframe>');
-	}
+  /* Create print frame if not exists */
+  if ('undefined' === typeof($('#' + o_cfg.id_frame).attr('id'))) {
+    $('body').append('<iframe id=\'' + o_cfg.id_frame + '\' \
+      name=\'' + o_cfg.id_frame + '\' \
+      width=\'0\' height=\'0\' frameborder=\'0\' \
+      src=\'about:blank\' ></iframe>');
+  }
 
-	/* Prepare css */
-	var s_css = '';
-	if (0 < o_cfg.css_url.length) {
-		if ('string' == typeof(o_cfg.css_url))
-			o_cfg.css_url = [o_cfg.css_url];
-		for (var i in o_cfg.css_url) {
-			s_css += '\
-				<link rel=\'stylesheet\' type=\'text/css\'\
-					media=\'print\'\
-					href=\'' + o_cfg.css_url[i] + '\'\
-				/>\
-			';
-		}
-	}
-	if (0 < o_cfg.css_text.length) {
-		s_css += '\
-			<style type=\'text/css\' media=\'print\'>\
-			' + o_cfg.css_text + '\
-			</style>\
-		';
-	}
+  /* Prepare css */
+  var s_css = '';
+  if (0 < o_cfg.css_url.length) {
+    if ('string' == typeof(o_cfg.css_url))
+      o_cfg.css_url = [o_cfg.css_url];
+    for (var i in o_cfg.css_url) {
+      s_css += '\
+        <link rel=\'stylesheet\' type=\'text/css\'\
+          media=\'print\'\
+          href=\'' + o_cfg.css_url[i] + '\'\
+        />\
+      ';
+    }
+  }
+  if (0 < o_cfg.css_text.length) {
+    s_css += '\
+      <style type=\'text/css\' media=\'print\'>\
+      ' + o_cfg.css_text + '\
+      </style>\
+    ';
+  }
 
-	/* Write content */
-	/* Use document.open/write fix FF need print twice problem. */
-	var o_f = document.frames ? document.frames[o_cfg.id_frame]
-		: document.getElementById(o_cfg.id_frame);
-	var o_fw = o_f.contentWindow || o_f;
-	o_fw.document.open('text/html', 'replace');
-	o_fw.document.write(s_css + $('#' + o_cfg.id).html());
-	o_fw.document.close();
+  /* Write content */
+  /* Use document.open/write fix FF need print twice problem. */
+  var o_f = document.frames ? document.frames[o_cfg.id_frame]
+    : document.getElementById(o_cfg.id_frame);
+  var o_fw = o_f.contentWindow || o_f;
+  o_fw.document.open('text/html', 'replace');
+  o_fw.document.write(s_css + $('#' + o_cfg.id).html());
+  o_fw.document.close();
 
-	/* Print */
-	o_fw.focus();
-	o_fw.print();
+  /* Print */
+  o_fw.focus();
+  o_fw.print();
 }
 
 
